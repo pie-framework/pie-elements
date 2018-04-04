c4149beb811ab5f0f5d7f2ec334687e8259db521
Harry <hsharma@primoko.com>
2018-04-04 14:02:58 +0530

    Configuration Added

--- packages/calculator/configure/src/main.jsx
+++ packages/calculator/configure/src/main.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import calculator from '@pie-ui/calculator';
+import Calculator from '@pie-framework/material-ui-calculator';
+import { InputRadio, TwoChoice } from '@pie-lib/config-ui';
 
 export default class Main extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      twoChoice: '',
+      model: props.model
+    };
   }
 
   render() {
-    return <calculator mode='basic' />;
+    const { basic, scientific } = this.state;
+    return (
+      <div>
+        <TwoChoice
+          header="Choose Calculator Type"
+          value={this.state.twoChoice}
+          onChange={twoChoice => this.setState({ twoChoice })}
+          one={{ label: 'Basic', value: 'basic' }}
+          two={{ label: 'Scientific', value: 'scientific' }} />
+          {this.state.twoChoice && <Calculator mode={this.state.twoChoice} />}
+      </div>
+    );
   }
 }
