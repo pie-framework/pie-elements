14c220657306622510d74d1ecfc40524ba7f6c55
edeustace <ed.eustace@gmail.com>
2018-04-11 12:10:43 -0400

    add some notes

--- packages/point-intercept/src/mapper.js
+++ packages/point-intercept/src/mapper.js
@@ -60,14 +60,31 @@ const modelMap = {
   showFeedback: ''
 };
 
+/**
+ * Convert old xy string array to points array
+ * @param {string[]} answers - eg: ['x,y', 'x,y']
+ * @param {*} oldModel
+ *
+ * @return {{x:number,y:number,label:string}[]}
+ */
 export function toSessionPoints(answers, oldModel) {
   return [];
 }
 
+/**
+ * Convert points array to xy string array
+ * @param {{x:number,y:number,label:string}[]} points
+ *
+ * @return {string[]}
+ */
 export function toSessionAnswers(points) {
   return [];
 }
 
+/**
+ * Convert model to one the @pie-ui/point-intercept/Component uses.
+ * @param {} m
+ */
 export function toComponentModel(m) {
   const newModel = Object.assign({}, m);
   const oldModelCopy = Object.assign({}, m);
