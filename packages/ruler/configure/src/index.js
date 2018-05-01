6dddb7937f14428608007556b0b907747ec641b1
edeustace <ed.eustace@gmail.com>
2018-05-01 14:23:00 -0400

    fix(dependencies: use @pie-ui/ruler@^1.2.1

--- packages/ruler/configure/src/index.js
+++ packages/ruler/configure/src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import Main from './main';
 import { ModelUpdatedEvent } from '@pie-framework/pie-configure-events';
 
+const defaultModel = () => ({
+  model: {
+    config: {
+      units: 'imperial',
+      length: 12,
+      ticks: 8,
+      pixelsPerUnit: 40,
+      label: 'in'
+    }
+  }
+});
+
 export default class RulerConfigure extends HTMLElement {
   connectedCallback() {
-    this.render();
+    setTimeout(() => {
+      if (!this._model) {
+        this._model = defaultModel();
+        this.dispatchEvent(new ModelUpdatedEvent(this._model, false));
+        this.render();
+      }
+    }, 100);
   }
 
   set model(m) {
+    console.log('m?', m);
     this._model = m;
     this.render();
   }
