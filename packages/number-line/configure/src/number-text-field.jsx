954d4c75f9a702f90d5d2abeec7d9489762e48b3
edeustace <ed.eustace@gmail.com>
2018-05-10 12:17:19 -0400

    add styled text field

---
+++ packages/number-line/configure/src/number-text-field.jsx
@@ -0,0 +1,24 @@
+import { NumberTextField as NTF } from '@pie-lib/config-ui';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+
+export class NumberTextField extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { classes } = this.props;
+    const props = { ...this.props, classes: undefined };
+    return <NTF {...props} className={classes.textField} />;
+  }
+}
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit
+  }
+});
+
+export default withStyles(styles)(NumberTextField);
