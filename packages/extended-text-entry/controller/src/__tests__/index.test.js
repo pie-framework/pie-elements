fe8f4eebfe91e5cfa0f06ef525bbe252935779bf
edeustace <ed.eustace@gmail.com>
2018-05-10 21:23:31 -0400

    chore(test): add tests

---
+++ packages/extended-text-entry/controller/src/__tests__/index.test.js
@@ -0,0 +1,46 @@
+import { model } from '../index';
+import isFunction from 'lodash/isFunction';
+
+const q = extras =>
+  Object.assign(
+    {
+      feedback: { type: 'default', default: 'this is default feedback' },
+      width: '500px',
+      height: '100px',
+      showMathInput: false
+    },
+    extras
+  );
+
+const s = extras => Object.assign({}, extras);
+
+const e = (mode = 'gather') => ({ mode });
+
+describe('controller', () => {
+  const assert = (label, question, session, env, expected) => {
+    it(label, async () => {
+      const result = await model(question, session, env);
+      if (isFunction(expected)) {
+        expected(result);
+      } else {
+        expect(result).toMatchObject(expected);
+      }
+    });
+  };
+
+  describe('model', () => {
+    describe('disabled', () => {
+      assert('gather', q(), s(), e(), { disabled: false });
+      assert('view', q(), s(), e('view'), { disabled: true });
+      assert('evaluate', q(), s(), e('evaluate'), { disabled: true });
+    });
+
+    describe('feedback', () => {
+      assert('none for gather', q(), s(), e(), { feedback: undefined });
+      assert('none for view', q(), s(), e('view'), { feedback: undefined });
+      assert('none for gather', q(), s(), e('evaluate'), {
+        feedback: q().feedback.default
+      });
+    });
+  });
+});
---
+++ packages/function-entry/controller/src/__tests__/index.test.js
@@ -0,0 +1,125 @@
+import { model } from '../index';
+
+describe('model', () => {
+  let result, question, session, env;
+
+  const mkQuestion = () => ({
+    equation: '3x+2',
+    feedback: {
+      correct: {
+        type: 'default'
+      },
+      incorrect: {
+        type: 'custom',
+        custom: 'foo'
+      }
+    }
+  });
+
+  describe('gather', () => {
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: '3x+2' };
+      env = { mode: 'gather' };
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:false', () => {
+      expect(result.disabled).toEqual(false);
+    });
+
+    it('returns undefined for correctness ', () => {
+      expect(result.correctness).toEqual(undefined);
+    });
+
+    it('returns undefined for feedback', () => {
+      expect(result.feedback).toEqual(undefined);
+    });
+  });
+
+  describe('view', () => {
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: '3x+2' };
+      env = { mode: 'view' };
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns undefined for correctness ', () => {
+      expect(result.correctness).toEqual(undefined);
+    });
+
+    it('returns default correct for feedback', () => {
+      expect(result.feedback).toEqual(undefined);
+    });
+  });
+
+  describe('evaluate - correct', () => {
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: '3x+2' };
+      env = { mode: 'evaluate' };
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns correct for correctness ', () => {
+      expect(result.correctness).toEqual('correct');
+    });
+
+    it('returns default correct for feedback', () => {
+      expect(result.feedback).toEqual('Correct');
+    });
+  });
+
+  describe('evaluate - incorrect', () => {
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: '3x+3' };
+      env = { mode: 'evaluate' };
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns incorrect for incorrectness ', () => {
+      expect(result.correctness).toEqual('incorrect');
+    });
+
+    it('returns custom incorrect for feedback', () => {
+      expect(result.feedback).toEqual('foo');
+    });
+  });
+
+  describe('evaluate - correctness with mathjs', () => {
+    it('returns (in)correct for mathematically (in)correct answer too', async () => {
+      question = mkQuestion();
+      session = { value: '3x+2+1-1+x-x' };
+      env = { mode: 'evaluate' };
+      result = await model(question, session, env);
+
+      expect(result.correctness).toEqual('correct');
+
+      session = { value: '3+3x+2-3' };
+      env = { mode: 'evaluate' };
+      result = await model(question, session, env);
+
+      expect(result.correctness).toEqual('correct');
+
+      session = { value: '3+3x+2-3+x' };
+      env = { mode: 'evaluate' };
+      result = await model(question, session, env);
+
+      expect(result.correctness).toEqual('incorrect');
+    });
+  });
+});
