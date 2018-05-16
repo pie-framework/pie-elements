705a4744cd37cbd1baad99937c37d57769d36722
edeustace <ed.eustace@gmail.com>
2018-05-16 10:23:37 -0400

    feat: upgrade dependencies

--- packages/calculator/configure/src/main.jsx
+++ packages/calculator/configure/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TwoChoice } from '@pie-lib/config-ui';
 import PropTypes from 'prop-types';
-import Typography from 'material-ui/Typography';
+import Typography from '@material-ui/core/Typography';
 import { CalculatorLayout } from '@pie-ui/calculator';
 
 const MainTypes = {
