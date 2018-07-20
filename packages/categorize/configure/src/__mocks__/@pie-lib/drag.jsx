9d15f6c9009c4204dcbde8223e234ec91e12bbc7
edeustace <ed.eustace@gmail.com>
2018-07-20 09:46:13 -0400

    chore: add mock for categorize tests

---
+++ packages/categorize/configure/src/__mocks__/@pie-lib/drag.jsx
@@ -0,0 +1,10 @@
+export const uid = {
+  withUid: jest.fn(input => input),
+  generateUid: jest.fn().mockReturnValue('1'),
+  Provider: jest.fn(c => c)
+};
+
+export const PlaceHolder = props => (
+  <div componentName="placeholder">{props.children}</div>
+);
+export const withDragContext = jest.fn(i => i);
