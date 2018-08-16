ec2853ed83ff977e55155dcd8aa3301f97bfcc67
edeustace <ed.eustace@gmail.com>
2018-08-15 21:36:45 -0400

    feat: add outcome method to controller

--- packages/multiple-choice/controller/src/__tests__/index.test.js
+++ packages/multiple-choice/controller/src/__tests__/index.test.js
@@ -1,11 +1,11 @@
-import { model } from '../index';
+import { model, outcome, scoreFromRule } from '../index';
 import { isResponseCorrect } from '../utils';
 
 jest.mock('../utils', () => ({
   isResponseCorrect: jest.fn()
 }));
 
-describe('model', () => {
+describe('controller', () => {
   let result, question, session, env;
 
   beforeEach(() => {
@@ -34,79 +34,141 @@ describe('model', () => {
     };
   });
 
-  describe('mode: gather', () => {
-    beforeEach(async () => {
-      session = {};
-      env = { mode: 'gather' };
-      result = await model(question, session, env);
+  describe('scoreFromRule', () => {
+    const assertScoreFromRule = (scorePercentage, expected, fallback) => {
+      it(`${scorePercentage} (fallback: ${fallback}) => ${expected}`, () => {
+        const result = scoreFromRule(
+          scorePercentage ? { scorePercentage } : null,
+          fallback
+        );
+        expect(result).toBeCloseTo(expected, 8);
+      });
+    };
+    assertScoreFromRule(80, 0.8);
+    assertScoreFromRule(70, 0.7);
+    assertScoreFromRule(null, 0.75, 0.75);
+  });
+
+  describe('outcome', () => {
+    it('returns score of 0', async () => {
+      const result = await outcome(
+        question,
+        { value: ['banana'] },
+        { mode: 'gather' }
+      );
+      expect(result.score).toEqual(0);
     });
 
-    it('returns disabled', () => {
-      expect(result.disabled).toEqual(false);
+    it('returns score of 1', async () => {
+      const result = await outcome(
+        question,
+        { value: ['apple'] },
+        { mode: 'gather' }
+      );
+      expect(result.score).toEqual(1);
     });
 
-    it('returns mode', () => {
-      expect(result.mode).toEqual('gather');
-    });
+    describe('partial scoring', () => {
+      beforeEach(() => {
+        const choices = question.choices.concat({ value: 'c', correct: true });
+        question = {
+          ...question,
+          partialScoring: [{ numberOfCorrect: 1, scorePercentage: 34.5 }],
+          choices
+        };
+      });
 
-    it('returns prompt', () => {
-      expect(result.prompt).toEqual('prompt');
-    });
+      it('returns a score of 0', async () => {
+        const result = await outcome(question, {}, {});
+        expect(result.score).toEqual(0);
+      });
 
-    it('returns choiceMode', () => {
-      expect(result.choiceMode).toEqual('radio');
-    });
-
-    it('returns keyMode', () => {
-      expect(result.keyMode).toEqual('letters');
-    });
-
-    it('returns complete', () => {
-      expect(result.complete).toEqual({ min: 1 });
-    });
-
-    it('returns choices', () => {
-      expect(result.choices).toEqual([
-        { label: 'a', value: 'apple' },
-        { label: 'b', value: 'banana' }
-      ]);
-    });
-
-    it('does not return responseCorrect', () => {
-      expect(result.responseCorrect).toBe(undefined);
+      it('returns score of 0.345', async () => {
+        const result = await outcome(
+          question,
+          { value: ['apple'] },
+          { mode: 'gather' }
+        );
+        expect(result.score).toBeCloseTo(0.345);
+      });
     });
   });
 
-  describe('mode: view', () => {
-    beforeEach(async () => {
-      session = {};
-      env = { mode: 'view' };
-      result = await model(question, session, env);
+  describe('model', () => {
+    describe('mode: gather', () => {
+      beforeEach(async () => {
+        session = {};
+        env = { mode: 'gather' };
+        result = await model(question, session, env);
+      });
+
+      it('returns disabled', () => {
+        expect(result.disabled).toEqual(false);
+      });
+
+      it('returns mode', () => {
+        expect(result.mode).toEqual('gather');
+      });
+
+      it('returns prompt', () => {
+        expect(result.prompt).toEqual('prompt');
+      });
+
+      it('returns choiceMode', () => {
+        expect(result.choiceMode).toEqual('radio');
+      });
+
+      it('returns keyMode', () => {
+        expect(result.keyMode).toEqual('letters');
+      });
+
+      it('returns complete', () => {
+        expect(result.complete).toEqual({ min: 1 });
+      });
+
+      it('returns choices', () => {
+        expect(result.choices).toEqual([
+          { label: 'a', value: 'apple' },
+          { label: 'b', value: 'banana' }
+        ]);
+      });
+
+      it('does not return responseCorrect', () => {
+        expect(result.responseCorrect).toBe(undefined);
+      });
     });
 
-    it('returns disabled', () => {
-      expect(result.disabled).toEqual(true);
-    });
-  });
+    describe('mode: view', () => {
+      beforeEach(async () => {
+        session = {};
+        env = { mode: 'view' };
+        result = await model(question, session, env);
+      });
 
-  describe('mode: evaluate', () => {
-    beforeEach(async () => {
-      session = {};
-      env = { mode: 'evaluate' };
-      isResponseCorrect.mockReturnValue(false);
-      result = await model(question, session, env);
-      return result;
+      it('returns disabled', () => {
+        expect(result.disabled).toEqual(true);
+      });
     });
 
-    it('returns choices w/ correct', () => {
-      expect(result.choices).toEqual([
-        { label: 'a', value: 'apple', correct: true, feedback: 'foo' },
-        { label: 'b', value: 'banana', correct: false, feedback: 'Incorrect' }
-      ]);
-    });
+    describe('mode: evaluate', () => {
+      beforeEach(async () => {
+        session = {};
+        env = { mode: 'evaluate' };
+        isResponseCorrect.mockReturnValue(false);
+        result = await model(question, session, env);
+        return result;
+      });
 
-    it('returns is response correct', () => {
-      expect(result.responseCorrect).toEqual(false);
+      it('returns choices w/ correct', () => {
+        expect(result.choices).toEqual([
+          { label: 'a', value: 'apple', correct: true, feedback: 'foo' },
+          { label: 'b', value: 'banana', correct: false, feedback: 'Incorrect' }
+        ]);
+      });
+
+      it('returns is response correct', () => {
+        expect(result.responseCorrect).toEqual(false);
+      });
     });
   });
 });
--- packages/multiple-choice/controller/src/index.js
+++ packages/multiple-choice/controller/src/index.js
@@ -1,12 +1,14 @@
-
 import { isResponseCorrect } from './utils';
+import debug from 'debug';
+import compact from 'lodash/compact';
 
-const prepareChoice = (mode, defaultFeedback) => (choice) => {
+const log = debug('pie-elements:multiple-choice:controller');
 
+const prepareChoice = (mode, defaultFeedback) => choice => {
   const out = {
     label: choice.label,
     value: choice.value
-  }
+  };
 
   if (mode == 'evaluate') {
     out.correct = !!choice.correct;
@@ -21,14 +23,17 @@ const prepareChoice = (mode, defaultFeedback) => (choice) => {
   }
 
   return out;
-}
-
+};
 
 export function model(question, session, env) {
   return new Promise((resolve, reject) => {
-
-    const defaultFeedback = Object.assign({ correct: 'Correct', incorrect: 'Incorrect' }, question.defaultFeedback);
-    const choices = question.choices.map(prepareChoice(env.mode, defaultFeedback));
+    const defaultFeedback = Object.assign(
+      { correct: 'Correct', incorrect: 'Incorrect' },
+      question.defaultFeedback
+    );
+    const choices = question.choices.map(
+      prepareChoice(env.mode, defaultFeedback)
+    );
 
     const out = {
       disabled: env.mode !== 'gather',
@@ -42,9 +47,53 @@ export function model(question, session, env) {
       complete: {
         min: question.choices.filter(c => c.correct).length
       },
-      responseCorrect: env.mode === 'evaluate' ? isResponseCorrect(question, session) : undefined
+      responseCorrect:
+        env.mode === 'evaluate'
+          ? isResponseCorrect(question, session)
+          : undefined
     };
 
     resolve(out);
+  });
+}
+
+const isCorrect = c => c.correct === true;
+
+const normalize = (n, min, max) => Math.max(min, Math.min(max, n));
+
+export const scoreFromRule = (rule, fallback) => {
+  if (!rule || !Number.isFinite(rule.scorePercentage)) {
+    return fallback;
+  }
+  return normalize(rule.scorePercentage, 0, 100) * 0.01;
+};
+
+export function outcome(config, session, env) {
+  return new Promise((resolve, reject) => {
+    log('outcome...');
+
+    const choices = compact(
+      (session.value || []).map(v => config.choices.find(c => c.value === v))
+    );
+    const correct = choices.filter(isCorrect);
+    const totalCorrect = config.choices.filter(isCorrect);
+    const allCorrect = correct.length === totalCorrect.length;
+    if (allCorrect) {
+      resolve({
+        score: 1
+      });
+    } else if (correct.length === 0) {
+      resolve({ score: 0 });
+    } else {
+      if (config.partialScoring) {
+        const rule = config.partialScoring.find(
+          ps => ps.numberOfCorrect === correct.length
+        );
+        const score = scoreFromRule(rule, correct / totalCorrect);
+        resolve({ score });
+      } else {
+        resolve({ score: 0 });
+      }
+    }
   });
 }
