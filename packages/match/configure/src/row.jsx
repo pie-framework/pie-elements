bfa2098bda4d64e6c9e9a510261c342375cd56b5
lakatosandrei <lakatosandrei@gmail.com>
2018-10-17 16:17:25 +0300

    feat(ch467): made it possible for rows to be reordered by drag and drop

---
+++ packages/match/configure/src/row.jsx
@@ -0,0 +1,220 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import { Checkbox } from '@pie-lib/config-ui';
+import DragHandle from '@material-ui/icons/DragHandle';
+import Input from '@material-ui/core/Input';
+import Radio from '@material-ui/core/Radio';
+import Button from '@material-ui/core/Button';
+import Delete from '@material-ui/icons/Delete';
+import { DragSource, DropTarget } from 'react-dnd';
+import debug from 'debug';
+
+const log = debug('@pie-element:categorize:configure:choice');
+
+let canDrag = false;
+
+export class Row extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    model: PropTypes.object.isRequired,
+    row: PropTypes.object.isRequired,
+    idx: PropTypes.number.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+    onDeleteRow: PropTypes.func.isRequired,
+    connectDragSource: PropTypes.func.isRequired,
+    connectDropTarget: PropTypes.func.isRequired,
+    onMoveRow: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {};
+
+  componentDidMount() {
+    document.addEventListener('mouseup', this.onMouseUpOnHandle);
+  }
+
+  onRowTitleChange = rowIndex => event => {
+    const { model, onChange } = this.props;
+    const newModel = { ...model };
+
+    newModel.rows[rowIndex].title = event.target.value;
+
+    onChange(newModel);
+  };
+
+  onRowValueChange = (rowIndex, rowValueIndex) => event => {
+    const { model, onChange } = this.props;
+    const newModel = { ...model };
+
+    if (model.responseType === 'radio') {
+      for (let i = 0; i < newModel.rows[rowIndex].values.length; i++) {
+        newModel.rows[rowIndex].values[i] = false;
+      }
+    }
+
+    newModel.rows[rowIndex].values[rowValueIndex] = event.target.checked;
+
+    onChange(newModel);
+  };
+
+  onDeleteRow = (idx) => () => {
+    this.props.onDeleteRow(idx)
+  };
+
+  onMouseDownOnHandle = () => {
+    canDrag = true;
+  };
+
+  onMouseUpOnHandle = () => {
+    canDrag = false;
+  };
+
+  render() {
+    const {
+      classes,
+      connectDragSource,
+      connectDropTarget,
+      isDragging,
+      model,
+      row,
+      idx
+    } = this.props;
+    const opacity = isDragging ? 0 : 1;
+
+    const content = (
+      <div style={{
+        opacity: opacity
+      }}>
+        <span
+          itemID={'handle'}
+          className={classes.dragHandle}
+          onMouseDown={this.onMouseDownOnHandle}
+        >
+          <DragHandle color={'primary'} />
+        </span>
+        <div className={classes.rowContainer}>
+          <div className={classNames(classes.rowItem, classes.questionText)}>
+            <Input
+              type="text"
+              disableUnderline
+              onChange={this.onRowTitleChange(idx)}
+              value={row.title}
+              placeholder="Enter Value"
+            />
+          </div>
+          {row.values.map((rowValue, rowIdx) => (
+            <div key={rowIdx} className={classes.rowItem}>
+              {model.responseType === 'radio' ? (
+                <Radio
+                  onChange={this.onRowValueChange(idx, rowIdx)}
+                  checked={rowValue === true}
+                />
+              ) : (
+                <Checkbox
+                  onChange={this.onRowValueChange(idx, rowIdx)}
+                  checked={rowValue === true}
+                />
+              )}
+            </div>
+          ))}
+          <div className={classes.deleteIcon}>
+            <Button onClick={this.onDeleteRow(idx)}>
+              <Delete className={classes.deleteIcon} />
+            </Button>
+          </div>
+        </div>
+        <hr className={classes.separator} />
+      </div>
+    );
+
+    return connectDragSource(connectDropTarget(content));
+  }
+}
+const styles = theme => ({
+  actions: {
+    padding: 0,
+    justifyContent: 'space-between'
+  },
+  choice: {
+    padding: theme.spacing.unit,
+    overflow: 'visible'
+  },
+  dragHandle: {
+    cursor: 'move'
+  },
+  dragDisabled: {
+    cursor: 'inherit'
+  },
+
+  container: {
+    marginTop: theme.spacing.unit * 2,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  rowContainer: {
+    marginTop: theme.spacing.unit * 2,
+    display: 'flex',
+    alignItems: 'center',
+    flex: 1
+  },
+  rowItem: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  deleteIcon: {
+    flex: 0.5,
+    minWidth: '88px',
+  },
+  questionText: {
+    flex: 2,
+    display: 'flex',
+    justifyContent: 'flex-start'
+  },
+  separator: {
+    marginTop: theme.spacing.unit * 2,
+    border: 0,
+    borderTop: '2px solid lightgray',
+    width: '100%'
+  }
+});
+
+const StyledRow = withStyles(styles)(Row);
+
+const NAME = 'row-config';
+
+const choiceSource = {
+  canDrag() {
+    return canDrag;
+  },
+  beginDrag(props) {
+    return {
+      id: props.row.id,
+      index: props.idx
+    };
+  }
+};
+
+const StyledSource = DragSource(NAME, choiceSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}))(StyledRow);
+
+const choiceTarget = {
+  hover() {
+    log('[hover]');
+  },
+  drop(props, monitor) {
+    const item = monitor.getItem();
+    log('[drop] item: ', item, 'didDrop?', monitor.didDrop());
+    props.onMoveRow(item.index, props.idx);
+  }
+};
+
+const StyledSourceAndTarget = DropTarget(NAME, choiceTarget, connect => ({
+  connectDropTarget: connect.dropTarget()
+}))(StyledSource);
+
+export default StyledSourceAndTarget;
