ad80818be5f937fd037a34cfb7e75566560048f7
ed eustace <ed.eustace@gmail.com>
2018-11-16 11:44:39 +0000

    feat: add generate module

---
+++ packages/match/docs/demo/generate.js
@@ -0,0 +1,45 @@
+exports.model = (id, element) => ({
+  id,
+  element,
+  rows: [
+    {
+      id: 1,
+      title: 'Question Text 1',
+      values: [false, false]
+    },
+    {
+      id: 2,
+      title: 'Question Text 2',
+      values: [false, false]
+    },
+    {
+      id: 3,
+      title: 'Question Text 3',
+      values: [false, false]
+    },
+    {
+      id: 4,
+      title: 'Question Text 4',
+      values: [false, false]
+    }
+  ],
+  shuffled: false,
+  partialScoring: false,
+  layout: 3,
+  headers: ['Column 1', 'Column 2', 'Column 3'],
+  responseType: 'radio',
+  feedback: {
+    correct: {
+      type: 'none',
+      default: 'Correct'
+    },
+    partial: {
+      type: 'none',
+      default: 'Nearly'
+    },
+    incorrect: {
+      type: 'none',
+      default: 'Incorrect'
+    }
+  }
+});
