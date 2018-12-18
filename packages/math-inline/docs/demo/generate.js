509c6618fccf7408997629b01d2106fc85a2339a
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-12-18 16:49:49 +0200

    feat(math-inline): generate.js for build-service

---
+++ packages/math-inline/docs/demo/generate.js
@@ -0,0 +1,38 @@
+exports.model = (id, element) => ({
+  id,
+  element,
+  mode: 'advanced',
+  expression: '1+1',
+  question: 'What is the equation for a slope?',
+  equationEditor: 'simple',
+  defaultResponse: {
+    id: 0,
+    validation: 'symbolic',
+    answer: 'y = mx + b',
+    alternates: {},
+    allowSpaces: true,
+    allowDecimals: true
+  },
+  responses: [{
+    id: 'answerBlock1',
+    validation: 'symbolic',
+    answer: 'y = mx + b',
+    alternates: {},
+    allowSpaces: true,
+    allowDecimals: true
+  }],
+  feedback: {
+    correct: {
+      type: 'none',
+      default: 'Correct'
+    },
+    partial: {
+      type: 'none',
+      default: 'Nearly'
+    },
+    incorrect: {
+      type: 'none',
+      default: 'Incorrect'
+    }
+  },
+});
