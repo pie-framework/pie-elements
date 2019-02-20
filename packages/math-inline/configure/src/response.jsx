d6e4a816c87e36622740991206d28195f955bec9
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-02-20 13:54:06 +0200

    fix(math-inline): keypad update for different types and keypad fixes

--- packages/math-inline/configure/src/response.jsx
+++ packages/math-inline/configure/src/response.jsx
@@ -80,7 +80,7 @@ class Response extends React.Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
     defaultResponse: PropTypes.bool,
-    mode: PropTypes.string,
+    mode: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     index: PropTypes.number,
     onResponseChange: PropTypes.func.isRequired,
     response: PropTypes.object.isRequired
@@ -88,7 +88,7 @@ class Response extends React.Component {
 
   static defaultProps = {
     defaultResponse: false,
-    mode: 'scientific'
+    mode: 'everything'
   };
 
   constructor(props) {
@@ -208,6 +208,8 @@ class Response extends React.Component {
     const { validation, answer, alternates, allowDecimals, allowSpaces } = response;
     const hasAlternates = Object.keys(alternates).length > 0;
 
+    console.log(mode);
+
     const classNames = {
       editor: classes.responseEditor
     };
@@ -255,6 +257,7 @@ class Response extends React.Component {
               <MathToolbar
                 classNames={classNames}
                 controlledKeypad
+                keypadMode={mode}
                 showKeypad={showKeypad[alternateId] || false}
                 latex={alternates[alternateId].answer}
                 onChange={this.onAlternateAnswerChange(alternateId)}
