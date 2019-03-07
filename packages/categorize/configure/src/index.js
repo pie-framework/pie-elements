c87a0a730ca8285f86c983e718eef594b3a0cacd
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-07 16:03:08 +0200

    chore(categorize): place default values in a separate file

--- packages/categorize/configure/src/index.js
+++ packages/categorize/configure/src/index.js
@@ -7,70 +7,16 @@ import {
   InsertImageEvent
 } from '@pie-framework/pie-configure-events';
 
-export default class CategorizeConfigure extends HTMLElement {
-  static prepareModelObject = (model = {}) => {
-    const sensibleDefaults = {
-      choices: [
-        {
-          id: '0',
-          content: '<span mathjax="" data-latex="">420\\text{ cm}=4.2\\text{ meters}</span>'
-        },
-        {
-          id: '1',
-          content: '<span mathjax="" data-latex="" data-raw="3.4\\text{ kg}=350\\text{ g}">3.4\\text{ kg}=340\\text{ g}</span>'
-        },
-      ],
-      categories: [
-        {
-          id: '0',
-          label: 'Equivalent',
-          choices: [
-            {
-              id: '0',
-              content: '<span mathjax="" data-latex="">420\\text{ cm}=4.2\\text{ meters}</span>'
-            },
-          ]
-        },
-        {
-          id: '1',
-          label: '<b>NOT </b>equivalent',
-          choices: [
-            {
-              id: '1',
-              content: '<span mathjax="" data-latex="">3.4\\text{ kg}=340\\text{ g}</span>'
-            },
-          ]
-        }
-      ],
-      correctResponse: [
-        {
-          category: '0',
-          choices: ['0']
-        },
-        {
-          category: '1',
-          choices: ['1']
-        }
-      ],
-      config: {
-        choices: {
-          columns: 2,
-          position: 'below',
-        },
-        categories: {
-          columns: 2
-        }
-      }
-    };
+import defaults from './defaults';
 
-    return {
-      ...sensibleDefaults,
-      ...model,
-    };
-  };
+export default class CategorizeConfigure extends HTMLElement {
+  static prepareModelObject = (model = {}) => ({
+    ...defaults,
+    ...model,
+  });
 
   set model(m) {
-    this._model = CategorizeConfigure.prepareModelObject(m);
+    this._model = CategorizeConfigure.prepareModelObject();
     this.render();
   }
 
--- packages/categorize/controller/src/index.js
+++ packages/categorize/controller/src/index.js
@@ -1,5 +1,6 @@
 import { buildState, score } from '@pie-lib/categorize';
 import { getFeedbackForCorrectness } from '@pie-lib/feedback';
+import defaults from './defaults';
 import debug from 'debug';
 const log = debug('@pie-element:categorize:controller');
 
@@ -35,40 +36,8 @@ export const getCorrectness = (question, session, env) => {
 
 export const createConfigModel = (model = {}) =>
   new Promise(resolve => {
-    const sensibleDefaults = {
-      choices: [
-        {
-          id: '0',
-          content: '420 cm = 4.2 meters'
-        },
-        {
-          id: '1',
-          content: '3.4 kg = 340 g'
-        },
-      ],
-      categories: [
-        {
-          id: '0',
-          label: 'Equivalent',
-        },
-        {
-          id: '1',
-          label: '<b>NOT </b>equivalent',
-        }
-      ],
-      config: {
-        choices: {
-          columns: 2,
-          position: 'below',
-        },
-        categories: {
-          columns: 2
-        }
-      }
-    };
-
     resolve({
-      ...sensibleDefaults,
+      ...defaults,
       ...model,
     })
   });
