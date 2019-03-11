168ee0e3d5e5f27352952350f29041113e6979ae
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-11 15:56:31 +0200

    feat: update the minimum default data necessary to prevent UI not being able to function

--- packages/math-inline/configure/src/defaults.js
+++ packages/math-inline/configure/src/defaults.js
@@ -3,10 +3,18 @@ export default {
   expression: 'y = ',
   question: '',
   equationEditor: 'everything',
+  defaultResponse: {
+    id: 0,
+    validation: 'symbolic',
+    answer: 'mx + b',
+    alternates: {},
+    allowDecimals: true
+  },
   responses: [{
     id: 'answerBlock1',
     validation: 'symbolic',
     answer: 'mx + b',
     alternates: {},
+    allowDecimals: true
   }],
 };
