61f4cf50c0612d81f91e9fc5c8a1ace93a4a0aea
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-11 15:43:22 +0200

    feat: update the minimum default data necessary to prevent UI not being able to function

--- packages/categorize/configure/src/defaults.js
+++ packages/categorize/configure/src/defaults.js
@@ -1,4 +1,44 @@
 export default {
-  categories: [],
-  choices: [],
+  choices: [
+    {
+      id: '0',
+      content: '<span mathjax="" data-latex="">420\\text{ cm}=4.2\\text{ meters}</span>'
+    },
+    {
+      id: '1',
+      content: '<span mathjax="" data-latex="" data-raw="3.4\\text{ kg}=350\\text{ g}">3.4\\text{ kg}=340\\text{ g}</span>'
+    },
+  ],
+  categories: [
+    {
+      id: '0',
+      label: 'Equivalent',
+      choices: [
+        {
+          id: '0',
+          content: '<span mathjax="" data-latex="">420\\text{ cm}=4.2\\text{ meters}</span>'
+        },
+      ]
+    },
+    {
+      id: '1',
+      label: '<b>NOT </b>equivalent',
+      choices: [
+        {
+          id: '1',
+          content: '<span mathjax="" data-latex="">3.4\\text{ kg}=340\\text{ g}</span>'
+        },
+      ]
+    }
+  ],
+  correctResponse: [
+    {
+      category: '0',
+      choices: ['0']
+    },
+    {
+      category: '1',
+      choices: ['1']
+    }
+  ],
 };
