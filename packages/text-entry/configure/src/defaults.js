942b2647cce9b1041826b70eabdd778dec21b0ad
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-11 20:11:54 +0200

    feat: implement default-model-initialization method

---
+++ packages/text-entry/configure/src/defaults.js
@@ -0,0 +1,18 @@
+export default {
+  correctResponses: {
+    values: ['mutt', 'hound'],
+    ignoreWhitespace: true,
+    ignoreCase: false
+  },
+  partialResponses: {
+    values: ['mutty'],
+    ignoreWhitespace: true,
+    ignoreCase: true,
+    awardPercentage: '50'
+  },
+  answerBlankSize: '10',
+  answerAlignment: 'left',
+  prompt: 'Question Prompt goes here',
+  allowDecimal: true,
+  allowThousandsSeparator: true
+};
