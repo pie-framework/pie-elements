04c7c33c48bde414f1fff121a0da1dc3c3a0dcfa
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-11 13:40:56 +0200

    feat: keep the minimum default data necessary to prevent UI not being able to function

--- packages/multiple-choice/configure/src/defaults.js
+++ packages/multiple-choice/configure/src/defaults.js
@@ -2,15 +2,5 @@ export default {
   prompt: 'Which of these northern European countries are EU members?',
   choiceMode: 'checkbox',
   keyMode: 'numbers',
-  choices: [
-    {
-      correct: true,
-      value: 'sweden',
-      label: 'Sweden',
-    },
-    {
-      value: 'iceland',
-      label: 'Iceland',
-    },
-  ],
+  choices: [],
 };
