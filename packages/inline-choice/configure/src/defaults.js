abf35e63b5ca6fa7d77a1fd372f80c93e05c962c
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-11 12:56:06 +0200

    feat: keep the minimum default data necessary to prevent UI not being able to function

--- packages/inline-choice/configure/src/defaults.js
+++ packages/inline-choice/configure/src/defaults.js
@@ -1,24 +1,3 @@
 export default {
-  choices: [
-    {
-      correct: true,
-      value: 'sweden',
-      label: 'Sweden'
-    },
-    {
-      value: 'iceland',
-      label: 'Iceland',
-      feedback: {
-        type: 'default'
-      }
-    },
-    {
-      value: 'finland',
-      label: 'Finland',
-      feedback: {
-        type: 'custom',
-        value: 'Nokia was founded in Finland.'
-      }
-    }
-  ]
+  choices: []
 };
