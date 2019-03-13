87b7febf3f567a56b00266b6603d7462f6d77551
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-13 18:07:56 +0200

    feat: stub ebsr pie element

---
+++ packages/ebsr/configure/src/index.js
@@ -0,0 +1,19 @@
+export default class EbsrConfigure extends HTMLElement {
+
+  constructor() {
+    super()
+  }
+
+  set model(m) {
+    this._model = m;
+    this.render();
+  }
+
+  render() {
+    if (!this._model) {
+      return;
+    }
+
+    this.innerHTML = `<pre>${JSON.stringify(this._model, null, '  ')}</pre>`
+  }
+}
---
+++ packages/ebsr/docs/demo/generate.js
@@ -0,0 +1,50 @@
+exports.model = (id, element) => ({
+  id,
+  element,
+  keyMode: 'numbers',
+  labelMode: 'numbers',
+  sequentialLabel: false,
+  lockChoiceOrder: false,
+  partA: {
+    prompt: 'What color is the sky?',
+    choiceMode: 'radio',
+    choices: [
+      {
+        value: 'yellow',
+        label: 'Yellow',
+      },
+      {
+        value: 'green',
+        label: 'Green',
+      },
+      {
+        correct: true,
+        value: 'blue',
+        label: 'Blue',
+      },
+    ]
+  },
+  partB: {
+    prompt: 'What color do you get when you mix Red with your answer in Part 1?',
+    choiceMode: 'radio',
+    choices: [
+      {
+        value: 'orange',
+        label: 'Orange',
+      },
+      {
+        correct: true,
+        value: 'purple',
+        label: 'Purple',
+      },
+      {
+        value: 'pink',
+        label: 'Pink',
+      },
+      {
+        value: 'green',
+        label: 'Green',
+      },
+    ]
+  },
+});
