198d130df7a8238c6dc55cddd9d39db4648a4849
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-15 09:16:08 +0200

    feat: use dummy default values

--- packages/match/configure/src/defaults.js
+++ packages/match/configure/src/defaults.js
@@ -5,11 +5,6 @@ export default {
       title: 'Question Text 1',
       values: [false, false]
     },
-    {
-      id: 2,
-      title: 'Question Text 2',
-      values: [false, false]
-    },
   ],
   layout: 3,
   headers: ['Column 1', 'Column 2', 'Column 3'],
