f32e0dc9ff01f489f2d064af9876b2248111a021
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-03-15 09:09:25 +0200

    feat: use dummy default values

--- packages/inline-choice/configure/src/defaults.js
+++ packages/inline-choice/configure/src/defaults.js
@@ -2,23 +2,8 @@ export default {
   choices: [
     {
       correct: true,
-      value: 'sweden',
-      label: 'Sweden'
-    },
-    {
-      value: 'iceland',
-      label: 'Iceland',
-      feedback: {
-        type: 'default'
-      }
-    },
-    {
-      value: 'finland',
-      label: 'Finland',
-      feedback: {
-        type: 'custom',
-        value: 'Nokia was founded in Finland.'
-      }
+      value: 'choice1',
+      label: 'Choice 1'
     }
   ]
 };
