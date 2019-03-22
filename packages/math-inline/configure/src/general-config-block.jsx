18bd7108a588f7177de40424862dcbc7145c7c1f
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-03-22 16:04:55 +0200

    fix: continued mathquill math-input work

--- packages/math-inline/configure/src/general-config-block.jsx
+++ packages/math-inline/configure/src/general-config-block.jsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import EditableHtml from '@pie-lib/editable-html';
 import { InputContainer } from '@pie-lib/config-ui';
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Response from './response';
 import { MathToolbar } from '@pie-lib/math-toolbar';
-import AnswerBlock from './answer-block';
+
+let registered = false;
 
 const styles = theme => ({
   container: {
@@ -53,6 +52,37 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 2,
     width: '100%',
     maxWidth: '600px'
+  },
+  blockContainer: {
+    margin: theme.spacing.unit,
+    display: 'inline-flex',
+    border: '2px solid grey'
+  },
+  blockResponse: {
+    flex: 2,
+    color: 'grey',
+    background: 'lightgrey',
+    fontSize: '0.8rem',
+    padding: theme.spacing.unit / 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRight: '2px solid grey'
+  },
+  blockMath: {
+    color: '#bdbdbd',
+    padding: theme.spacing.unit / 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 8,
+    '& > .mq-math-mode': {
+      '& > .mq-hasCursor': {
+        '& > .mq-cursor': {
+          display: 'none'
+        },
+      }
+    }
   }
 });
 
@@ -113,23 +143,59 @@ class GeneralConfigBlock extends React.Component {
     newModel.responses = newModel.responses.concat(response);
     onChange(newModel);
 
-    this.handleAnswerBlockDomUpdate(answerBlockId, newModel.responses.length - 1, '', true);
+    setTimeout(this.handleAnswerBlockDomUpdate(), 0);
   };
 
-  handleAnswerBlockDomUpdate = (answerBlockId, index, latex) => {
-    const container = document.getElementById(answerBlockId);
-    const element = (
-      <AnswerBlock
-        id={answerBlockId}
-        index={index}
-        latex={latex}
-      />
-    );
+  UNSAFE_componentWillMount() {
+    const { classes } = this.props;
 
-    if (container) {
-      ReactDOM.render(element, document.getElementById(answerBlockId));
-    } else {
-      setTimeout(() => this.handleAnswerBlockDomUpdate(answerBlockId, index, latex), 0);
+    if (typeof window !== 'undefined') {
+      const MathQuill = require('mathquill');
+      let MQ = MathQuill.getInterface(2);
+
+      if (!registered) {
+        MQ.registerEmbed('answerBlock', data => {
+          return {
+            htmlString: `<div class="${classes.blockContainer}">
+                <div class="${classes.blockResponse}" id="${data}Index">R</div>
+                <div class="${classes.blockMath}">
+                  <span id="${data}"></span>
+                </div>
+              </div>`,
+            text: () => 'text',
+            latex: () => `\\embed{answerBlock}[${data}]`
+          };
+        });
+
+        registered = true;
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.handleAnswerBlockDomUpdate();
+  }
+
+  componentDidUpdate() {
+    this.handleAnswerBlockDomUpdate();
+  }
+
+  handleAnswerBlockDomUpdate = () => {
+    const { model } = this.props;
+
+    if (this.root && model.responses.length) {
+      model.responses.forEach((response, idx) => {
+        const el = this.root.querySelector(`#${response.id}`);
+        const indexEl = this.root.querySelector(`#${response.id}Index`);
+
+        if (el) {
+          const MathQuill = require('mathquill');
+          let MQ = MathQuill.getInterface(2);
+          el.textContent = response.answer;
+          MQ.StaticMath(el);
+          indexEl.textContent = `R${idx + 1}`;
+        }
+      })
     }
   };
 
@@ -179,7 +245,7 @@ class GeneralConfigBlock extends React.Component {
     };
 
     return (
-      <div className={classes.container}>
+      <div ref={r => (this.root = r || this.root)} className={classes.container}>
         <InputContainer label="Item Type" className={classes.selectContainer}>
           <Select
             className={classes.select}
@@ -193,7 +259,7 @@ class GeneralConfigBlock extends React.Component {
         <InputContainer label="Item Stem" className={classes.promptHolder}>
           <EditableHtml
             className={classes.prompt}
-            markup={model.question}
+            markup={question}
             onChange={this.onChange('question')}
             imageSupport={imageSupport}
             nonEmpty={false}
