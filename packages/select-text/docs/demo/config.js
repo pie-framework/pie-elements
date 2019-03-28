71521d2f3038e08c6b057b57639c8d9a77e0ffcd
andreeapescar <andreeaflaviapescar@gmail.com>
2019-03-28 12:47:18 +0200

    Added few new models to test html.

--- packages/select-text/docs/demo/config.js
+++ packages/select-text/docs/demo/config.js
@@ -1,8 +1,8 @@
-const { model, mathSample } = require('./generate');
+const { model, mathSample, htmlSample, htmlSample2 } = require('./generate');
 
 module.exports = {
   elements: {
     'select-text': '../..'
   },
-  models: [model('1', 'select-text'), mathSample('2', 'select-text')]
+  models: [model('1', 'select-text'), mathSample('2', 'select-text'), htmlSample('3', 'select-text'), htmlSample2('4', 'select-text')]
 };
