cdb29e41d8e6ea550f70fb4c719f3f18afffbed9
andreeapescar <andreeaflaviapescar@gmail.com>
2019-04-18 09:03:28 +0300

    feat(extended-text-entry): Updated model structure with new established structure, integrated settings panel and updated tests.

--- packages/extended-text-entry/configure/src/defaults.js
+++ packages/extended-text-entry/configure/src/defaults.js
@@ -1,5 +1,41 @@
 export default {
+  height: 100,
   prompt: 'This is the question prompt',
+  mathInput: false,
   width: 500,
-  height: 100
+  configure: {
+    equationEditor: {
+      settings: false,
+      label: 'Equation Editor',
+      enabled: false,
+    },
+    height: {
+      settings: true,
+      label: 'Height (px)',
+    },
+    mathInput: {
+      settings: true,
+      label: 'Student response can include math notation',
+      enabled: false,
+    },
+    multipleParts: {
+      settings: false,
+      label: 'Multiple Parts',
+      enabled: false,
+    },
+    studentInstructions: {
+      settings: false,
+      label: 'Student Instructions',
+      enabled: true,
+    },
+    teacherInstructions: {
+      settings: false,
+      label: 'Teacher Instructions',
+      enabled: false,
+    },
+    width: {
+      settings: true,
+      label: 'Width (px)',
+    },
+  }
 };
