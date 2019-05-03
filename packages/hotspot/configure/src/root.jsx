036a9d84b94eb2ec3db4e8a888309388268efff3
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-05-03 18:55:17 +0300

    Ed's CRs [ch1194]

--- packages/hotspot/configure/src/root.jsx
+++ packages/hotspot/configure/src/root.jsx
@@ -1,104 +1,20 @@
-import Main from './main';
 import React from 'react';
-import cloneDeep from 'lodash/cloneDeep';
 import PropTypes from 'prop-types';
 
+import Main from './main';
+
 class Root extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      model: props.model,
-      disableSidePanel: props.disableSidePanel
-    };
-  }
-
-  UNSAFE_componentWillReceiveProps(props) {
-    const { disableSidePanel } = props;
-    const { disableSidePanel: oldDisableProp } = this.props;
-
-    if (disableSidePanel !== oldDisableProp) {
-      this.setState({
-        disableSidePanel
-      });
-    }
-  }
-
-  updateModel = (model, reset) => {
-    this.setState({ model }, () => {
-      this.modelChanged(reset);
-    });
-  };
-
-  modelChanged = (reset) => {
-    this.props.onModelChanged(this.state.model, reset);
-  };
-
-  onRemoveShape = index => {
-    const { model } = this.state;
-    model.shapes.splice(index, 1);
-    this.updateModel(model);
-  };
-
-  onColorChanged = (colorType, color) => {
-    const { model } = this.state;
-    model[colorType] = color;
-    this.updateModel(model);
-  };
-
-  onPromptChanged = prompt => {
-    const update = cloneDeep(this.state.model);
-    update.prompt = prompt;
-    this.updateModel(update);
-  };
-
-  onPartialScoringChanged = () => {
-    const { model } = this.state;
-    model.partialScoring = !model.partialScoring;
-    this.updateModel(model);
-  };
-
-  onMultipleCorrectChanged = () => {
-    const { model } = this.state;
-    model.multipleCorrect = !model.multipleCorrect;
-    if (!model.multipleCorrect) {
-      model.partialScoring = false;
-    }
-    model.shapes = model.shapes.map(shape => ({ ...shape, correct: false }));
-    this.updateModel(model);
-  };
-
-  onUpdateImageDimension = (dimensions) => {
-    const { model } = this.state;
-    model.dimensions = dimensions;
-    this.updateModel(model);
-  };
-
-  onUpdateShapes = (shapes) => {
-    const { model } = this.state;
-    model.shapes = shapes;
-    this.updateModel(model);
-  };
-
-  onImageUpload = imageUrl => {
-    const { model } = this.state;
-    model.imageUrl = imageUrl;
-    this.updateModel(model);
-  };
-
   render() {
     const props = {
       configure: this.props.configure,
-      disableSidePanel: this.state.disableSidePanel,
-      model: this.state.model,
-      onColorChanged: this.onColorChanged,
-      onImageUpload: this.onImageUpload,
-      onMultipleCorrectChanged: this.onMultipleCorrectChanged,
-      onPartialScoringChanged: this.onPartialScoringChanged,
-      onPromptChanged: this.onPromptChanged,
-      onRemoveShape: this.onRemoveShape,
-      onUpdateImageDimension: this.onUpdateImageDimension,
-      onUpdateShapes: this.onUpdateShapes
+      disableSidePanel: this.props.disableSidePanel,
+      model: this.props.model,
+      onImageUpload: this.props.onImageUpload,
+      onColorChanged: this.props.onColorChanged,
+      onPartialScoringChanged: this.props.onPartialScoringChanged,
+      onPromptChanged: this.props.onPromptChanged,
+      onUpdateImageDimension: this.props.onUpdateImageDimension,
+      onUpdateShapes: this.props.onUpdateShapes
     };
 
     return <Main {...props} />;
@@ -109,7 +25,12 @@ Root.propTypes = {
   configure: PropTypes.object,
   disableSidePanel: PropTypes.bool,
   model: PropTypes.object.isRequired,
-  onModelChanged: PropTypes.func.isRequired
+  onImageUpload: PropTypes.func.isRequired,
+  onColorChanged: PropTypes.func.isRequired,
+  onPartialScoringChanged: PropTypes.func.isRequired,
+  onPromptChanged: PropTypes.func.isRequired,
+  onUpdateImageDimension: PropTypes.func.isRequired,
+  onUpdateShapes: PropTypes.func.isRequired
 };
 
 export default Root;
