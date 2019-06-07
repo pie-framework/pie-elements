41e210ebc6d761041efb1d632a1879f35b1487a1
lakatosandrei <lakatosandrei@gmail.com>
2019-06-07 23:05:19 +0300

    feat(mask-markup-elements): enabled partial scoring for the 3 elements and added a controller to the match-list

---
+++ packages/match-list/controller/src/defaults.js
@@ -0,0 +1,18 @@
+export default {
+  config: {
+    rows: [
+      {
+        id: 1,
+        title: 'Question Text 1',
+        values: [false, false]
+      },
+      {
+        id: 2,
+        title: 'Question Text 2',
+        values: [false, false]
+      },
+    ],
+    layout: 3,
+    headers: ['Column 1', 'Column 2', 'Column 3'],
+  }
+};
