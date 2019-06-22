850e05e3510841a8c858b1dea6c78058336a8133
ed eustace <ed.eustace@gmail.com>
2019-06-22 23:32:45 +0100

    rm old functions

--- packages/number-line/configure/src/index.js
+++ packages/number-line/configure/src/index.js
@@ -21,119 +21,101 @@ export default class NumberLineConfigReactElement extends HTMLElement {
     this._rerender();
   }
 
-  onDomainChanged(domain) {
-    this._model.graph.domain = domain;
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onDomainChanged(domain) {
+  //   this._model.graph.domain = domain;
+  //   let detail = {
+  //     update: this._model
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onTickFrequencyChange(event, value) {
-    this._model.graph.tickFrequency = parseInt(value, 10);
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onTickFrequencyChange(event, value) {
+  //   this._model.graph.tickFrequency = parseInt(value, 10);
+  //   let detail = {
+  //     update: this._model
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onMinorTicksChanged(event, value) {
-    this._model.graph.showMinorTicks = value;
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onMinorTicksChanged(event, value) {
+  //   this._model.graph.showMinorTicks = value;
+  //   let detail = {
+  //     update: this._model
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onSnapPerTickChange(event, value) {
-    this._model.graph.snapPerTick = parseInt(value, 10);
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onSnapPerTickChange(event, value) {
+  //   this._model.graph.snapPerTick = parseInt(value, 10);
+  //   let detail = {
+  //     update: this._model
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onConfigChange(graph) {
-    this._model.graph = graph;
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onCorrectResponseChange(correctResponse) {
+  //   this.onChange({correctResponse});
+  // }
 
-  onCorrectResponseChange(correctResponse) {
-    this._model.correctResponse = correctResponse;
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onInitialElementsChange(initialElements) {
+  //   this._model.graph.initialElements = initialElements;
+  //   let detail = {
+  //     update: this._model,
+  //     reset: true
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onInitialElementsChange(initialElements) {
-    this._model.graph.initialElements = initialElements;
-    let detail = {
-      update: this._model,
-      reset: true
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onAvailableTypesChange(availableTypes) {
+  //   this._model.graph.availableTypes = availableTypes;
+  //   let detail = {
+  //     update: this._model,
+  //     reset: true
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onAvailableTypesChange(availableTypes) {
-    this._model.graph.availableTypes = availableTypes;
-    let detail = {
-      update: this._model,
-      reset: true
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onFeedbackChange(feedback) {
+  //   this._model.feedback = feedback;
+  //   let detail = {
+  //     update: this._model
+  //   };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
+  //   this._rerender();
+  // }
 
-  onFeedbackChange(feedback) {
-    this._model.feedback = feedback;
-    let detail = {
-      update: this._model
-    };
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-    this._rerender();
-  }
+  // onPromptChanged = prompt => {
+  //   this._model.prompt = prompt;
+  //   let detail = {
+  //     update: this._model
+  //   };
 
-  onPromptChanged = prompt => {
-    this._model.prompt = prompt;
-    let detail = {
-      update: this._model
-    };
+  //   this.dispatchEvent(
+  //     new CustomEvent('model.updated', { bubbles: true, detail })
+  //   );
 
-    this.dispatchEvent(
-      new CustomEvent('model.updated', { bubbles: true, detail })
-    );
-
-    this._rerender();
-  };
+  //   this._rerender();
+  // };
 
   onChange = o => {
     console.log('onChange: ', o);
@@ -148,16 +130,12 @@ export default class NumberLineConfigReactElement extends HTMLElement {
       model: this._model,
       configuration: this._configuration,
       onChange: this.onChange,
-      onDomainChange: this.onDomainChanged.bind(this),
-      onMinorTicksChanged: this.onMinorTicksChanged.bind(this),
-      onTickFrequencyChange: this.onTickFrequencyChange.bind(this),
-      onSnapPerTickChange: this.onSnapPerTickChange.bind(this),
-      onConfigChange: this.onConfigChange.bind(this),
-      onCorrectResponseChange: this.onCorrectResponseChange.bind(this),
-      onInitialElementsChange: this.onInitialElementsChange.bind(this),
-      onAvailableTypesChange: this.onAvailableTypesChange.bind(this),
-      onFeedbackChange: this.onFeedbackChange.bind(this),
-      onPromptChanged: this.onPromptChanged.bind(this)
+      onDomainChange: this.onDomainChanged.bind(this)
+      // onCorrectResponseChange: this.onCorrectResponseChange.bind(this),
+      // onInitialElementsChange: this.onInitialElementsChange.bind(this),
+      // onAvailableTypesChange: this.onAvailableTypesChange.bind(this),
+      // onFeedbackChange: this.onFeedbackChange.bind(this),
+      // onPromptChanged: this.onPromptChanged.bind(this)
     });
     ReactDOM.render(element, this);
   }
