81b49b35253a9983e473f7228edb47de419a7f49
ed eustace <ed.eustace@gmail.com>
2019-06-24 11:25:22 +0100

    fix: bump @pie-ui/number-line to ^7.1.0

--- packages/number-line/configure/src/defaults.js
+++ packages/number-line/configure/src/defaults.js
@@ -1,35 +1,38 @@
 export const model = {
-  domain: { min: -5, max: 5 },
-  ticks: {
-    minor: 1,
-    major: 2
-  },
-  arrows: {
-    left: true,
-    right: true
-  },
-  maxNumberOfPoints: 20,
-  width: 500,
-  height: 400,
-  initialType: 'PF',
-  exhibitOnly: false,
-  availableTypes: {
-    PF: true,
-    LFF: true,
-    LEF: true,
-    LFE: true,
-    LEE: true,
-    RFN: true,
-    RFP: true,
-    REN: true,
-    REP: true
-  },
-  initialElements: []
+  correctResponse: [],
+  graph: {
+    domain: { min: -5, max: 5 },
+    ticks: {
+      minor: 1,
+      major: 2
+    },
+    arrows: {
+      left: true,
+      right: true
+    },
+    maxNumberOfPoints: 20,
+    width: 500,
+    height: 400,
+    initialType: "PF",
+    exhibitOnly: false,
+    availableTypes: {
+      PF: true,
+      LFF: true,
+      LEF: true,
+      LFE: true,
+      LEE: true,
+      RFN: true,
+      RFP: true,
+      REN: true,
+      REP: true
+    },
+    initialElements: []
+  }
 };
 
 export const configuration = {
   prompt: {
     settings: true,
-    label: 'Prompt'
+    label: "Prompt"
   }
 };
