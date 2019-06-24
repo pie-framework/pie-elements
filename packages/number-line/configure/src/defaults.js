54da4d70d306fa5dc89fc52876b333a7b37e493d
ed eustace <ed.eustace@gmail.com>
2019-06-24 10:01:00 +0100

    feat: update pie-ui/number-line

--- packages/number-line/configure/src/defaults.js
+++ packages/number-line/configure/src/defaults.js
@@ -1,31 +1,35 @@
-export default {
-  model: {
-    prompt: 'Question Prompt goes here.',
-    correctResponse: [],
-    graph: {
-      domain: [-5, 5],
-      initialElements: [],
-      showMinorTicks: true,
-      initialType: 'PF',
-      exhibitOnly: false,
-      availableTypes: {
-        PF: true,
-        PE: false,
-        LFF: false,
-        LEF: false,
-        LFE: false,
-        LEE: false,
-        RFN: false,
-        RFP: false,
-        REN: false,
-        REP: false
-      }
-    }
+export const model = {
+  domain: { min: -5, max: 5 },
+  ticks: {
+    minor: 1,
+    major: 2
   },
-  configuration: {
-    prompt: {
-      settings: true,
-      label: 'Prompt'
-    }
+  arrows: {
+    left: true,
+    right: true
+  },
+  maxNumberOfPoints: 20,
+  width: 500,
+  height: 400,
+  initialType: 'PF',
+  exhibitOnly: false,
+  availableTypes: {
+    PF: true,
+    LFF: true,
+    LEF: true,
+    LFE: true,
+    LEE: true,
+    RFN: true,
+    RFP: true,
+    REN: true,
+    REP: true
+  },
+  initialElements: []
+};
+
+export const configuration = {
+  prompt: {
+    settings: true,
+    label: 'Prompt'
   }
 };
