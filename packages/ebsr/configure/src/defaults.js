23cdb930e2375684d006b637ba363bf16173f5a9
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-07-10 19:07:17 +0300

    feat: support configuration

--- packages/ebsr/configure/src/defaults.js
+++ packages/ebsr/configure/src/defaults.js
@@ -18,6 +18,57 @@ export default {
     prompt: 'prompt b'
   },
   configuration: {
+    addChoiceButton: {
+      settings: true,
+      label: 'Add a Choice',
+    },
+    choiceMode: {
+      settings: true,
+      label: 'Response Type'
+    },
+    choicePrefix: {
+      settings: true,
+      label: 'Choice Labels'
+    },
+    deleteChoice: {
+      settings: true,
+    },
+    feedback: {
+      settings: true,
+      label: 'Feedback',
+      enabled: true
+    },
+    prompt: {
+      settings: true,
+      label: 'Prompt'
+    },
+    lockChoiceOrder: {
+      settings: true,
+      label: 'Lock Choice Order'
+    },
+    partialScoring: {
+      settings: true,
+      label: 'Allow Partial Scoring',
+    },
+    rationale: {
+      settings: true,
+      label: 'Rationale',
+      enabled: false,
+    },
+    scoringType: {
+      settings: false,
+      label: 'Scoring Type',
+    },
+    studentInstructions: {
+      settings: false,
+      label: 'Student Instructions',
+      enabled: true,
+    },
+    teacherInstructions: {
+      settings: true,
+      label: 'Teacher Instructions',
+      enabled: true,
+    },
     sequentialChoiceLabels: {
       settings: false,
       label: 'Sequential Choice Labels',
