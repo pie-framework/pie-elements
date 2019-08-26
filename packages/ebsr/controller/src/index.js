764c50fa286961132ecf3c3a65aa5e488ca3e909
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-08-26 19:08:39 +0300

    feat: make use of updateSession callback

--- packages/ebsr/controller/src/index.js
+++ packages/ebsr/controller/src/index.js
@@ -1,9 +1,13 @@
-import debug from 'debug';
 import shuffle from 'lodash/shuffle';
 import defaults from './defaults';
 import { isResponseCorrect } from './utils';
+import compact from 'lodash/compact';
 
-const log = debug('pie-elements:ebsr:controller');
+const lg = n => console[n].bind(console, '[ebsr:controller]');
+const debug = lg('debug');
+const log = lg('log');
+const warn = lg('warn');
+const error = lg('error');
 
 const prepareChoice = (model, env, defaultFeedback) => choice => {
   const out = {
@@ -60,7 +64,53 @@ const parsePart = (part, key, session, env) => {
   }
 };
 
-export function model(question, session, env) {
+const getShuffledChoices = async (choices, session, updateSession) => {
+  log('updateSession type: ', typeof updateSession);
+  log('session: ', session);
+  if (session.shuffledValues) {
+    debug('use shuffledValues to sort the choices...', session.shuffledValues);
+
+    return compact(
+      session.shuffledValues.map(v => choices.find(c => c.value === v))
+    );
+  } else {
+    const shuffledChoices = shuffle(choices);
+
+    if (updateSession && typeof updateSession === 'function') {
+      try {
+        //Note: session.id refers to the id of the element within a session
+        const shuffledValues = shuffledChoices.map(c => c.value);
+        log('try to save shuffledValues to session...', shuffledValues);
+        console.log('call updateSession... ', session.id, session.element);
+        await updateSession(session.id, session.element, { shuffledValues });
+      } catch (e) {
+        warn('unable to save shuffled order for choices');
+        error(e);
+      }
+    } else {
+      warn('unable to save shuffled choices, shuffle will happen every time.');
+    }
+    //save this shuffle to the session for later retrieval
+    return shuffledChoices;
+  }
+};
+
+/**
+ *
+ * @param {*} question
+ * @param {*} session
+ * @param {*} env
+ * @param {*} updateSession - optional - a function that will set the properties passed into it on the session.
+ */
+export async function model(question, session, env, updateSession) {
+  if (!question.partA.lockChoiceOrder) {
+    question.partA.choices = await getShuffledChoices(question.partA.choices, session, updateSession);
+  }
+
+  if (!question.partB.lockChoiceOrder) {
+    question.partB.choices = await getShuffledChoices(question.partB.choices, session, updateSession);
+  }
+
   return new Promise((resolve) => {
     resolve({
       disabled: env.mode !== 'gather',
