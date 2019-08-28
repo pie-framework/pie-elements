f4735bd40f02a3b8e07c8c0d8b0b357ea1bfe48b
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-08-28 18:24:19 +0300

    use shuffle/persistence method from pie-lib

--- packages/match/controller/src/index.js
+++ packages/match/controller/src/index.js
@@ -1,13 +1,9 @@
-import { getFeedbackForCorrectness } from '@pie-lib/feedback';
 import _ from 'lodash';
-import shuffle from 'lodash/shuffle';
-import compact from 'lodash/compact';
+import { getFeedbackForCorrectness } from '@pie-lib/feedback';
+import { getShuffledChoices } from '@pie-lib/controller-utils';
 
 const lg = n => console[n].bind(console, '[match]');
-const debug = lg('debug');
 const log = lg('log');
-const warn = lg('warn');
-const error = lg('error');
 
 import defaults from './defaults';
 
@@ -134,37 +130,6 @@ export function createDefaultModel(model = {}) {
   });
 }
 
-const getShuffledChoices = async (choices, session, updateSession) => {
-  log('updateSession type: ', typeof updateSession);
-  log('session: ', session);
-  if (session.shuffledValues) {
-    debug('use shuffledValues to sort the choices...', session.shuffledValues);
-
-    return compact(
-      session.shuffledValues.map(id => choices.find(c => c.id === id))
-    );
-  } else {
-    const shuffledChoices = shuffle(choices);
-
-    if (updateSession && typeof updateSession === 'function') {
-      try {
-        //Note: session.id refers to the id of the element within a session
-        const shuffledValues = shuffledChoices.map(c => c.id);
-        log('try to save shuffledValues to session...', shuffledValues);
-        console.log('call updateSession... ', session.id, session.element);
-        await updateSession(session.id, session.element, { shuffledValues });
-      } catch (e) {
-        warn('unable to save shuffled order for choices');
-        error(e);
-      }
-    } else {
-      warn('unable to save shuffled choices, shuffle will happen every time.');
-    }
-    //save this shuffle to the session for later retrieval
-    return shuffledChoices;
-  }
-};
-
 /**
  *
  * @param {*} question
@@ -184,7 +149,7 @@ export function model(question, session, env, updateSession) {
 
 
     if (!question.lockChoiceOrder) {
-      question.rows = await getShuffledChoices(question.rows, session, updateSession);
+      question.rows = await getShuffledChoices(question.rows, session, updateSession, 'id');
     }
 
     question.rows.forEach(row => {
