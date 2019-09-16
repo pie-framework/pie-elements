220a051e1e1a45e0278cf6936e6ebf3641c8362c
andreeapescar <andreeaflaviapescar@gmail.com>
2019-09-16 20:21:03 +0300

    Moved rationale, teacherInstructions and studentInstructions enabled flag in model because it's needed in controller.
    BREAKING CHANGE: `enabled` property from `configuration.rationale.enabled` moved to `model.rationaleEnabled`. Same applies to teacherInstructions/studentInstructions.

--- packages/pie-models/src/pie/placement-ordering/index.ts
+++ packages/pie-models/src/pie/placement-ordering/index.ts
@@ -97,6 +97,15 @@ export interface PlacementOrderingPie extends PieModel {
 
     /** Indicates teacher instructions */
     teacherInstructions?: string;
+
+    /** Indicates if Rationale are enabled */
+    rationaleEnabled: boolean;
+
+    /** Indicates if Student Instructions are enabled */
+    studentInstructionsEnabled: boolean;
+
+    /** Indicates if Teacher Instructions are enabled */
+    teacherInstructionsEnabled: boolean;
 }
 
 /**
