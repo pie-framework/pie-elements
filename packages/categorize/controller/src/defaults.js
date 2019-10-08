22f04f025b99d397c803676cc8daa4e9fb5777c8
andreeapescar <andreeaflaviapescar@gmail.com>
2019-10-08 11:00:14 +0300

    Remove unnecessary change.

--- packages/categorize/controller/src/defaults.js
+++ packages/categorize/controller/src/defaults.js
@@ -1,19 +1,4 @@
 export default {
   choices: [],
-  choicesPerRow: 2,
-  choicesPosition: 'below',
-  choicesLabel: '',
-  lockChoiceOrder: true,
-  removeTilesAfterPlacing: false,
-  categoriesPerRow: 2,
-  categories: [],
-  alternates: [],
-  correctResponse: [],
-  rowLabels: [''],
-  partialScoring: true,
-  feedbackEnabled: true,
-  rationaleEnabled: true,
-  promptEnabled: true,
-  teacherInstructionsEnabled: true,
-  studentInstructionsEnabled: true
+  categories: []
 };
