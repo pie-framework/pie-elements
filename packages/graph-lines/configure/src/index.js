a7ac839ccb47ee056949769d0a06e5ab51611edd
andreeapescar <andreeaflaviapescar@gmail.com>
2019-10-08 13:06:00 +0300

    fix(graph-lines): Added normalize function on controller (to be able to add default values to the model).

--- packages/graph-lines/configure/src/index.js
+++ packages/graph-lines/configure/src/index.js
@@ -25,7 +25,6 @@ export default class GraphLinesConfigure extends HTMLElement {
   set model(m) {
     this._model = GraphLinesConfigure.createDefaultModel(m);
     this._render();
-    this.dispatchEvent(new ModelUpdatedEvent(this._model));
   }
 
   set configuration(c) {
