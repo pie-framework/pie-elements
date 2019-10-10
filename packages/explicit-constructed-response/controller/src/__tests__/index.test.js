85257a1d38cd09b0167a68ba0a841518b5012e91
ed eustace <ed.eustace@gmail.com>
2019-10-10 11:44:19 +0100

    test it wip 2

--- packages/explicit-constructed-response/controller/src/__tests__/index.test.js
+++ packages/explicit-constructed-response/controller/src/__tests__/index.test.js
@@ -122,9 +122,9 @@ describe('prepareVal', () => {
   });
 });
 
-describe('null choices', () => {
+describe('null choices - ch4723', () => {
   it.only('..', async () => {
-    const item = {
+    const question = {
       markup:
         "<p>Jan's shape has {{0}} rows.</p>\n\n<p>Jan's shape has {{1}} columns.</p>\n\n<p>Jan used {{2}} squares in all to make the shape.</p>\n",
       choices: {
@@ -163,5 +163,47 @@ describe('null choices', () => {
 
     const result = await model(question, session, jest.fn());
     console.log('result:', result);
+
+    //Note: not sure if this assertion is totally correct - this is more just to highlight the issue.
+    expect(result.choices).toEqual(question.choices);
+  });
+});
+
+const d = {
+  prompt: '',
+  teacherInstructions: true,
+  studentInstructions: false,
+  choices: {
+    0: [choice('A', '0'), choice('B', '1')]
+  },
+  markup:
+    '<p>\n  <strong>Esta cuadrícula de coordenadas representa el diseño del salón de matemáticas de Valerie. Cada unidad en la cuadricula representa 1 pie cuadrado.&nbsp;</strong>\n</p>\n<p>\n  <strong>* El escritorio de Valeria se encuentra en (-3,-4)</strong>\n</p>\n<p>\n  <strong>* El escritorio de la maestra se encuentra en (5,-4)</strong>\n</p>\n<p>\n  <strong>* El cuarto de materiales se encuentra&nbsp; en (-3,-7)</strong>\n</p>\n<p>\n  <strong><img src="https://storage.googleapis.com/pie-staging-221718-assets/image/180d28b2-0376-428e-b415-de96e158b7e7" alt=""></strong>\n</p>\n<p>\n  &nbsp;\n</p>\n<p>\n  <strong>Anota la distancia, en pies, desde el escritorio de Valerie al cuarto de materiales.</strong>\n</p>\n<p>\n  {{0}} <strong>pies</strong>\n</p>',
+  id: '2422330',
+  rubric: '',
+  element: 'explicit-constructed-response-element',
+  autoScoring: 'partial',
+  rationale: ''
+};
+
+describe('edge cases', () => {
+  it('does not throw an error if the session has no value', async () => {
+    const result = model(d, {}, { mode: 'evaluate' });
+    expect(result).toBeDefined();
+  });
+
+  describe('model - with updateSession', () => {
+    it('calls updateSession', async () => {
+      const session = { id: '1', element: 'explicit-constructed-response' };
+      const env = { mode: 'gather' };
+      const updateSession = jest.fn().mockResolvedValue();
+      await model(d, session, env, updateSession);
+      expect(updateSession).toHaveBeenCalledWith(
+        '1',
+        'explicit-constructed-response',
+        {
+          shuffledValues: expect.arrayContaining(['0', '1'])
+        }
+      );
+    });
   });
 });
