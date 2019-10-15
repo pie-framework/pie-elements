83d8e222560186ad38ea4fc23d9bfd034b787a6c
ed eustace <ed.eustace@gmail.com>
2019-10-15 10:30:04 +0100

    add session

--- packages/match/docs/demo/session.js
+++ packages/match/docs/demo/session.js
@@ -1,6 +1,12 @@
 module.exports = [
   {
     id: '1',
-    element: 'match',
-  },
+    element: 'match-element',
+    answers: {
+      1: [true, false],
+      2: [false, true],
+      3: [true, false],
+      4: [false, true]
+    }
+  }
 ];
