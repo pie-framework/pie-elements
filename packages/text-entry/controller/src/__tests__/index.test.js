109453c2e0acf3143e5555a776a19292b44825eb
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-10-16 16:27:35 +0300

    chore: formatting

--- packages/text-entry/controller/src/__tests__/index.test.js
+++ packages/text-entry/controller/src/__tests__/index.test.js
@@ -1,4 +1,9 @@
-import { model, outcome, getCorrectness, createCorrectResponseSession } from '../index';
+import {
+  model,
+  outcome,
+  getCorrectness,
+  createCorrectResponseSession
+} from '../index';
 
 const mkQuestion = () => ({
   correctResponses: {
@@ -101,11 +106,14 @@ describe('model', () => {
         mode: 'gather',
         role: 'instructor'
       });
-      expect(sess).toEqual({"id": "1", "value": "a" });
+      expect(sess).toEqual({ id: '1', value: 'a' });
     });
 
     it('returns null env is student', async () => {
-      const noResult = await createCorrectResponseSession(question, { mode: 'gather', role: 'student' });
+      const noResult = await createCorrectResponseSession(question, {
+        mode: 'gather',
+        role: 'student'
+      });
       expect(noResult).toBeNull();
     });
   });
@@ -134,7 +142,9 @@ describe('model', () => {
 
 describe('outcome', () => {
   const assertOutcome = session => {
-    it(`returns score: 0, empty: true if session is ${JSON.stringify(session)}`, async () => {
+    it(`returns score: 0, empty: true if session is ${JSON.stringify(
+      session
+    )}`, async () => {
       const o = await outcome(mkQuestion(), session, { mode: 'evaluate' });
       expect(o).toEqual({ score: 0, empty: true });
     });
@@ -147,7 +157,9 @@ describe('outcome', () => {
 
 describe('getCorrectness', () => {
   const assertCorrectness = session => {
-    it(`returns score: 0, empty: true if session is ${JSON.stringify(session)}`, () => {
+    it(`returns score: 0, empty: true if session is ${JSON.stringify(
+      session
+    )}`, () => {
       const o = getCorrectness(mkQuestion(), session, { mode: 'evaluate' });
       expect(o).toEqual('empty');
     });
