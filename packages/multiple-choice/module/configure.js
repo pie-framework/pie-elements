5142cd468109f1c0bcc760e92df95c45fc36b651
ed eustace <ed.eustace@gmail.com>
2020-03-23 09:45:47 +0000

    wip

--- packages/multiple-choice/module/configure.js
+++ packages/multiple-choice/module/configure.js
@@ -1,2 +1,6 @@
 import { _dll_react } from '@pie-ui/shared-lib@1.0.0/index.js';
+import {
+  dataConverter,
+  NumberLineComponent
+} from '@pie-ui/number-line@.../module/index.js';
 // ...
--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -1,47 +1,27 @@
 module.exports = {
-  packages: [
-    {
-      type: 'pie-element-pkg',
-      name: '@pie-element/multiple-choice'
-    }
-  ],
-  libs: [
-    // set up a remote lib
-    // how to make sure that the deps in the remote version work for this repo?
-    { name: '@pie-ui/shared-lib', version: '???' }
-
-    // OR...
-    /**
-     * for each package if they have a pie.element say mc@1.x.x
-     * then get a list of bm deps that that module uses and add them as a lib
-     *
-     * - will need to get the pm to list the browser module it uses...
-     *
-     * - still need to version check?
-     */
-  ]
+  pkg: {
+    type: 'pie-package'
+  },
+  packages: ['@pie-element/number-line']
 };
 
 /**
- * open pkg.json
- *
- * open pie
- *
+ * building element:
+ *   open pkg.json, open pie
  *   if element - then we just need to point to the module url so : @pie-ui/mc@^4.11.12/module/index.js in a js file?
  *   if configure/controller - same?
  *
- *   building configure ...
+ * building configure:
  *   say cfg depends on pie ui - we want to mark that as an extrnal, then redirect it to the remote url
  *   also use the libs from any pie-ui el..
  *   - create any additional libs here
  *
- *   building controller - this is a node compatible + bm compatibale el - so should be a straight build (cant do a bm dependency) - so straight up
+ * building controller:
+ * this is a node compatible + bm compatibale el - so should be a straight build (cant do a bm dependency) - so straight up
  *
+ * (deployment): on npm publish - set up a hook that will pull the controller into a cf or lambda
  *
- *
+ * cmd usage:
  *   yarn pslb -c pslb.config.js
- *
- *
- *   > on npm publish - set up a hook that will pull the controller into a cf or lambda
  *
  */
