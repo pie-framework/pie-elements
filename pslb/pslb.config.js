6f19664916a68b8a45dbf4636ab10aaed4596404
ed eustace <ed.eustace@gmail.com>
2020-03-24 14:56:51 +0000

    build tweaks wip

--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -1,13 +1,146 @@
 const path = require('path');
+
+const reactIsExports = [
+  'AsyncMode',
+  'ConcurrentMode',
+  'ContextConsumer',
+  'ContextProvider',
+  'Element',
+  'ForwardRef',
+  'Fragment',
+  'Lazy',
+  'Memo',
+  'Portal',
+  'Profiler',
+  'StrictMode',
+  'Suspense',
+  'isAsyncMode',
+  'isConcurrentMode',
+  'isContextConsumer',
+  'isContextProvider',
+  'isElement',
+  'isForwardRef',
+  'isFragment',
+  'isLazy',
+  'isMemo',
+  'isPortal',
+  'isProfiler',
+  'isStrictMode',
+  'isSuspense',
+  'isValidElementType',
+  'typeOf'
+];
+
+const konva = [
+  'Layer',
+  'FastLayer',
+  'Group',
+  'Label',
+  'Rect',
+  'Circle',
+  'Ellipse',
+  'Wedge',
+  'Transformer',
+  'Line',
+  'Sprite',
+  'Image',
+  'Text',
+  'TextPath',
+  'Star',
+  'Ring',
+  'Arc',
+  'Tag',
+  'Path',
+  'RegularPolygon',
+  'Arrow',
+  'Shape',
+  'useStrictMode',
+  'Stage'
+];
+
+const commonJs = {
+  namedExports: {
+    'node_modules/react-konva/lib/ReactKonva.js': konva,
+    'node_modules/react-redux/node_modules/react-is/index.js': reactIsExports,
+    //TODO: common js should be picking these up?
+    'node_modules/react-is/index.js': reactIsExports,
+    'node_modules/react/index.js': [
+      'memo',
+      'useLayoutEffect',
+      'useEffect',
+      'useState',
+      'useCallback',
+      'useContext',
+      'cloneElement',
+      'createElement',
+      'createContext',
+      'isValidElement',
+      'useMemo',
+      'useRef',
+      'createRef',
+      'Component'
+    ],
+    'node_modules/react-dom/server.browser.js': ['renderToStaticMarkup'],
+    'node_modules/react-dom/index.js': ['findDOMNode'],
+    'node_modules/esrever/esrever.js': ['reverse'],
+    'node_modules/immutable/dist/immutable.js': [
+      'Map',
+      'Set',
+      'List',
+      'Iterable',
+      'Seq',
+      'Collection',
+      'OrderedMap',
+      'Stack',
+      'OrderedSet',
+      'Record',
+      'Range',
+      'Repeat',
+      'is',
+      'fromJS'
+    ]
+  }
+};
+
 module.exports = {
   pkg: {
     type: 'pie-package',
     // eslint-disable-next-line no-undef
     root: path.resolve(__dirname, '../packages'),
     output: '$pkg/module/index.js',
-    extensions: {}
+    extensions: { commonJs }
   },
-  packages: ['@pie-element/number-line']
+  packages: ['@pie-element/number-line'],
+  libs: [
+    {
+      name: '@pie-element/shared-config',
+      // eslint-disable-next-line no-undef
+      output: path.resolve(__dirname, '../packages'),
+      minify: false,
+      mode: 'development',
+      extensions: {
+        commonJs
+      },
+      /**
+       * Ideally namespace imports would be the default import method.
+       * But this can cause problems if a library does the following:
+       * `module.exports = require("path");` - this causes the properties to get lost,
+       * when really we'd like all the properties to be set on the module.
+       * For now - specify the import method as a key of `imports`.
+       *
+       * To fix this we'd probably need to make a change to rollup,
+       * get it to follow the var until it gets to the object definition,
+       * then to use the keys to set the export object.
+       *
+       * @see rollup/src/ast/NamespaceVariable
+       * @see rollup/plugin-commonjs/ too
+       */
+      imports: {
+        defaults: [],
+        namespace: ['@pie-framework/pie-configure-events', '@pie-lib/config-ui']
+      }
+    }
+  ]
 };
 
 /**
