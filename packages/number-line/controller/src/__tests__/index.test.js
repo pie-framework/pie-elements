33d0b83065e981cbff16522de32617add85c5896
ed eustace <ed.eustace@gmail.com>
2020-04-13 21:44:03 +0100

    fix: corret closeTo precision PD-132 (#446)

--- packages/number-line/controller/src/__tests__/index.test.js
+++ packages/number-line/controller/src/__tests__/index.test.js
@@ -2,49 +2,49 @@ import _ from 'lodash';
 import { defaults } from '@pie-lib/feedback';
 import * as controller from '../index';
 
-const mkQuestion = extras => ({
+const mkQuestion = (extras) => ({
   correctResponse: [
     {
       type: 'point',
       pointType: 'full',
-      position: 1
+      position: 1,
     },
     {
       type: 'ray',
       direction: 'left',
       position: 1,
-      pointType: 'empty'
-    }
+      pointType: 'empty',
+    },
   ],
   graph: {
-    domain: { min: 0, max: 1 }
+    domain: { min: 0, max: 1 },
   },
   feedback: {
     correct: {
-      type: 'default'
+      type: 'default',
     },
     incorrect: {
-      type: 'default'
+      type: 'default',
     },
     partial: {
-      type: 'default'
-    }
+      type: 'default',
+    },
   },
-  ...extras
+  ...extras,
 });
 
 const correctSession = {
-  answer: mkQuestion().correctResponse
+  answer: mkQuestion().correctResponse,
 };
 
 const incorrectSession = {
-  answer: [{ type: 'point', pointType: 'empty', position: 2 }]
+  answer: [{ type: 'point', pointType: 'empty', position: 2 }],
 };
 const partiallyCorrect = {
-  answer: [mkQuestion().correctResponse[0]]
+  answer: [mkQuestion().correctResponse[0]],
 };
 
-const mode = m => ({ mode: m });
+const mode = (m) => ({ mode: m });
 
 describe('controller', () => {
   describe('closeTo', () => {
@@ -54,6 +54,22 @@ describe('controller', () => {
         expect(controller.closeTo(a, b, precision)).toEqual(isClose);
       });
     };
+
+    it.each`
+      a                     | b             | expected
+      ${0.1}                | ${0.1}        | ${true}
+      ${0.1}                | ${0.11}       | ${false}
+      ${0.1}                | ${0.101}      | ${false}
+      ${0.1}                | ${0.1001}     | ${true}
+      ${0.1}                | ${0.09}       | ${false}
+      ${0.1}                | ${0.099}      | ${false}
+      ${0.1}                | ${0.0999}     | ${true}
+      ${0.4166666666666667} | ${0.66666664} | ${false}
+    `('$a closeTo $b is $expected', ({ a, b, expected }) => {
+      const result = controller.closeTo(a, b, 3); //controller.CLOSE_TO_PRECISION);
+      console.log('result:', result);
+      expect(result).toBe(expected);
+    });
 
     assertCloseTo(1, 1, true);
     assertCloseTo(1, 1.000000001, true);
@@ -76,6 +92,56 @@ describe('controller', () => {
     assertCloseTo(1, 1.1, false, 1);
   });
 
+  describe('PD-132', () => {
+    it('the score is 0', async () => {
+      const question = {
+        prompt:
+          '<div>An inn has two floors and has one&#8211; and two&#8211;person rooms. &#160;The two&#8211;way table below represents all the rooms at the inn.<br /><br /><table class="KdsTable01"><tr><td width="100px">&#160;</td><td width="100px">First Floor</td><td width="100px">Second Floor</td></tr><tr>  <td>One&#8211;Person Room</td>  <td>5</td>  <td>25</td></tr><tr>  <td>Two&#8211;Person Room</td>  <td>10</td>  <td>20</td></tr></table><br />On the number line below, plot the probability that a room selected at random is a two&#8211;person room given that it is on the first floor.</div>',
+        correctResponse: [
+          {
+            pointType: 'full',
+            domainPosition: 0.66666664,
+            type: 'point',
+          },
+        ],
+        id: '8a80808163e9a4a80163f04f2232357f',
+        graph: {
+          availableTypes: {
+            PF: true,
+          },
+          maxNumberOfPoints: 1,
+          height: 300,
+          initialElements: [],
+          width: 500,
+          initialType: 'PF',
+          tickLabelOverrides: ['1/6', '1/3', '1/2', '2/3', '5/6'],
+          domain: {
+            max: 1,
+            min: 0,
+          },
+          ticks: {
+            major: 0.08333333,
+            minor: 0.08333333,
+          },
+        },
+        element: 'number-line',
+      };
+      const session = {
+        answer: [
+          {
+            pointType: 'full',
+            domainPosition: 0.4166666666666667,
+            type: 'point',
+          },
+        ],
+        id: '8a80808163e9a4a80163f04f2232357f',
+      };
+      const env = { mode: 'evaluate' };
+      const result = await controller.outcome(question, session, env);
+      expect(result.score).toEqual(0);
+    });
+  });
+
   describe('outcome', () => {
     const assertOutcome = (label, question, session, env, expected) => {
       it(label, async () => {
@@ -91,18 +157,18 @@ describe('controller', () => {
             {
               pointType: 'full',
               type: 'point',
-              domainPosition: 0.41666664999999997
-            }
-          ]
+              domainPosition: 0.41666664999999997,
+            },
+          ],
         };
         const s = {
           answer: [
             {
               pointType: 'full',
               type: 'point',
-              domainPosition: 0.4166666666666667
-            }
-          ]
+              domainPosition: 0.4166666666666667,
+            },
+          ],
         };
         const e = { mode: 'evaluate' };
         const result = await controller.outcome(q, s, e);
@@ -116,8 +182,8 @@ describe('controller', () => {
       {
         answer: [
           ...mkQuestion().correctResponse,
-          { type: 'point', pointType: 'full', position: -3.3 }
-        ]
+          { type: 'point', pointType: 'full', position: -3.3 },
+        ],
       },
       {},
       { score: 0.5 }
@@ -194,14 +260,14 @@ describe('controller', () => {
       it(msg, () => {
         return controller
           .model(question, session, env)
-          .then(o => {
+          .then((o) => {
             if (_.isFunction(expected)) {
               expected(o);
             } else {
               expect(o).toMatchObject(expected);
             }
           })
-          .catch(e => {
+          .catch((e) => {
             throw new Error('Create Model spec error');
           });
       });
@@ -215,7 +281,7 @@ describe('controller', () => {
       {
         graph: {},
         disabled: true,
-        colorContrast: 'black_on_white'
+        colorContrast: 'black_on_white',
       }
     );
 
@@ -225,7 +291,7 @@ describe('controller', () => {
         {},
         {},
         { mode: 'gather' },
-        r => {
+        (r) => {
           expect(r.disabled).toBe(undefined);
         }
       );
@@ -234,12 +300,12 @@ describe('controller', () => {
         'disabled is true if exhibitOnly is true',
         {
           graph: {
-            exhibitOnly: true
-          }
+            exhibitOnly: true,
+          },
         },
         {},
         { mode: 'gather' },
-        r => {
+        (r) => {
           expect(r.disabled).toBe(true);
         }
       );
@@ -253,8 +319,8 @@ describe('controller', () => {
         {},
         {
           graph: {
-            domain: { min: -1, max: 1 }
-          }
+            domain: { min: -1, max: 1 },
+          },
         }
       );
     });
@@ -269,8 +335,8 @@ describe('controller', () => {
           corrected: {
             correct: [0, 1],
             incorrect: [],
-            notInAnswer: []
-          }
+            notInAnswer: [],
+          },
         }
       );
 
@@ -282,8 +348,8 @@ describe('controller', () => {
         {
           corrected: {
             correct: [],
-            incorrect: [0]
-          }
+            incorrect: [0],
+          },
         }
       );
     });
@@ -294,7 +360,7 @@ describe('controller', () => {
         {},
         correctSession,
         mode('evaluate'),
-        o => expect(o.correctResponse).toBe(undefined)
+        (o) => expect(o.correctResponse).toBe(undefined)
       );
 
       assertModel(
@@ -311,8 +377,8 @@ describe('controller', () => {
         assertModel(fbType, {}, s, mode('evaluate'), {
           feedback: {
             type: fbType,
-            message: defaults[fbType].default
-          }
+            message: defaults[fbType].default,
+          },
         });
       };
       assertFeedback(correctSession, 'correct');
@@ -321,14 +387,14 @@ describe('controller', () => {
     });
 
     describe('color contrast', () => {
-      const assertDefault = c => {
+      const assertDefault = (c) => {
         assertModel(
           `returns ${c}`,
           mkQuestion,
           {},
           { accessibility: { colorContrast: c } },
           {
-            colorContrast: c
+            colorContrast: c,
           }
         );
       };
@@ -338,11 +404,19 @@ describe('controller', () => {
     });
 
     describe('session not set', () => {
-      const assertModel = sess => {
-        it(`returns feedback unknown if session is ${JSON.stringify(sess)}`, async () => {
-          const m = await controller.model(mkQuestion(), sess, { mode: 'evaluate' });
+      const assertModel = (sess) => {
+        it(`returns feedback unknown if session is ${JSON.stringify(
+          sess
+        )}`, async () => {
+          const m = await controller.model(mkQuestion(), sess, {
+            mode: 'evaluate',
+          });
           expect(m).toMatchObject({
-            feedback: { type: 'unanswered', message: defaults.unanswered.default }});
+            feedback: {
+              type: 'unanswered',
+              message: defaults.unanswered.default,
+            },
+          });
         });
       };
 
@@ -354,14 +428,19 @@ describe('controller', () => {
 
   describe('correct response', () => {
     it('returns correct response if env is correct', async () => {
-      const sess = await controller.createCorrectResponseSession(mkQuestion(), { mode: 'gather', role: 'instructor' });
-      expect(sess).toEqual({...correctSession, ...{id: "1"}});
+      const sess = await controller.createCorrectResponseSession(mkQuestion(), {
+        mode: 'gather',
+        role: 'instructor',
+      });
+      expect(sess).toEqual({ ...correctSession, ...{ id: '1' } });
     });
 
     it('returns null env is student', async () => {
-      const noResult = await controller.createCorrectResponseSession(mkQuestion(), { mode: 'gather', role: 'student' });
+      const noResult = await controller.createCorrectResponseSession(
+        mkQuestion(),
+        { mode: 'gather', role: 'student' }
+      );
       expect(noResult).toBeNull();
     });
   });
-
 });
