3adf64017175fb4fdb116187303dbe7a1640f924
ed eustace <ed.eustace@gmail.com>
2020-04-28 14:40:14 +0100

    fix math-inline test

--- packages/select-text/configure/src/__tests__/design.test.jsx
+++ packages/select-text/configure/src/__tests__/design.test.jsx
@@ -5,16 +5,16 @@ import defaultValues from '../defaultConfiguration';
 
 jest.mock('@pie-lib/config-ui', () => ({
   layout: {
-    ConfigLayout: props => <div>{props.children}</div>
+    ConfigLayout: (props) => <div>{props.children}</div>,
   },
   settings: {
-    Panel: props => <div oChangeModel={props.onChange} />,
+    Panel: (props) => <div oChangeModel={props.onChange} />,
     toggle: jest.fn(),
-    radio: jest.fn()
-  }
+    radio: jest.fn(),
+  },
 }));
 
-jest.useFakeTimers();
+jest.mock('lodash/debounce', () => (fn) => fn);
 
 describe('design', () => {
   let w;
@@ -48,20 +48,23 @@ describe('design', () => {
     });
 
     it('tokenizer renders with html entities', () => {
-      expect(shallow(
-        <Design
-          model={{
-            text: "<p>&#8220;Lucy?&#63; Are you using your time wisely to plan your project?&#33;&#33;&#33;&#8221; Mr. Wilson asked.<\/p><p>Lucy looked a little confused at first. &#195; Then she grinned and proudly stated, &#8220;Why, yes I am! I plan to make a bird feeder for that tree out our window!&#8221;<\/p>",
-            tokens: []
-          }}
-          configuration={defaultValues.configuration}
-          classes={{}}
-          className={'foo'}
-          onModelChanged={onChange}
-          onPromptChanged={onPromptChanged}
-          onRationaleChanged={onRationaleChanged}
-        />
-      )).toMatchSnapshot();
+      expect(
+        shallow(
+          <Design
+            model={{
+              text:
+                '<p>&#8220;Lucy?&#63; Are you using your time wisely to plan your project?&#33;&#33;&#33;&#8221; Mr. Wilson asked.</p><p>Lucy looked a little confused at first. &#195; Then she grinned and proudly stated, &#8220;Why, yes I am! I plan to make a bird feeder for that tree out our window!&#8221;</p>',
+              tokens: [],
+            }}
+            configuration={defaultValues.configuration}
+            classes={{}}
+            className={'foo'}
+            onModelChanged={onChange}
+            onPromptChanged={onPromptChanged}
+            onRationaleChanged={onRationaleChanged}
+          />
+        )
+      ).toMatchSnapshot();
     });
 
     it('renders all items except feedback', () => {
@@ -113,16 +116,16 @@ describe('design', () => {
         const instance = w.instance();
         instance[fn].apply(instance, args);
 
-        jest.runAllTimers();
+        //jest.runAllTimers();
 
         expect(onChange).toBeCalledWith(e);
       });
     };
 
     describe('changeText', () => {
-      assert('changeText', [{ target: { value: 'foo' } }], m => ({
+      assert('changeText', [{ target: { value: 'foo' } }], (m) => ({
         ...m,
-        text: 'foo'
+        text: 'foo',
       }));
     });
 
@@ -130,65 +133,69 @@ describe('design', () => {
       assert(
         'changeTokens',
         [[{ start: 0, end: 1, text: 'f' }], 'words'],
-        m => ({
+        (m) => ({
           ...m,
           maxSelections: 0,
           tokens: [{ start: 0, end: 1, text: 'f' }],
-          mode: 'words'
+          mode: 'words',
         })
       );
       assert(
         'changeTokens',
         [[{ start: 0, end: 8, text: 'Foo bar.', correct: true }], 'sentences'],
-        m => ({
+        (m) => ({
           ...m,
           maxSelections: 1,
           tokens: [{ start: 0, end: 8, text: 'Foo bar.', correct: true }],
-          mode: 'sentences'
+          mode: 'sentences',
         })
       );
     });
 
     describe('changeMaxSelections', () => {
-      assert('changeMaxSelections', [{}, 4], m => ({ ...m, maxSelections: 4 }));
+      assert('changeMaxSelections', [{}, 4], (m) => ({
+        ...m,
+        maxSelections: 4,
+      }));
     });
 
     describe('changeFeedback', () => {
-      assert('changeFeedback', [{ correctFeedbackType: 'none' }], m => ({
+      assert('changeFeedback', [{ correctFeedbackType: 'none' }], (m) => ({
         ...m,
-        feedback: { correctFeedbackType: 'none' }
+        feedback: { correctFeedbackType: 'none' },
       }));
     });
 
     describe('changePartialScoring', () => {
-      assert('changePartialScoring', [true], m => ({
+      assert('changePartialScoring', [true], (m) => ({
         ...m,
-        partialScoring: true
+        partialScoring: true,
       }));
     });
 
     describe('changePrompt', () => {
-      assert('onPromptChanged', ['New Prompt'], m => ({
+      assert('onPromptChanged', ['New Prompt'], (m) => ({
         ...m,
-        prompt: 'New Prompt'
+        prompt: 'New Prompt',
       }));
     });
 
     describe('changeTeacherInstructions', () => {
-      assert('onTeacherInstructionsChanged', ['New Teacher Instructions'], m => ({
-        ...m,
-        teacherInstructions: 'New Teacher Instructions'
-      }));
+      assert(
+        'onTeacherInstructionsChanged',
+        ['New Teacher Instructions'],
+        (m) => ({
+          ...m,
+          teacherInstructions: 'New Teacher Instructions',
+        })
+      );
     });
-
 
     describe('changeRationale', () => {
-      assert('onRationaleChanged', ['New Rationale'], m => ({
+      assert('onRationaleChanged', ['New Rationale'], (m) => ({
         ...m,
-        rationale: 'New Rationale'
+        rationale: 'New Rationale',
       }));
     });
-
-
   });
 });
