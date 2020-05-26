60027b6d7652c3d4f07832ddbd582b725f25c26c
ed eustace <ed.eustace@gmail.com>
2020-05-26 15:51:47 +0100

    fix: update config for pslb@^3.0.0

--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
 const _ = require('lodash');
-// math-inline - 404 - maybe wasnt published
-// graphing - ?? - bad model
 const reactIsExports = [
   'AsyncMode',
   'ConcurrentMode',
@@ -145,84 +143,49 @@ const listPackages = () => {
 
 module.exports = {
   packages: listPackages(),
-  commonLib: {
-    packageDir: path.resolve(__dirname, '../packages'),
-    minify: false,
-    mode: 'development',
+  packagesDir: path.resolve(__dirname, '../packages'),
+  type: 'pie-package',
+  extensions: {
+    commonJs,
+  },
+  mode: 'development',
+  range: 'exact',
+  minify: false,
+  libs: {
     repository: 'pie-framework/pie-elements',
-    extensions: {
-      commonJs,
-    },
-  },
-  pkg: {
-    type: 'pie-package',
-    // eslint-disable-next-line no-undef
-    root: path.resolve(__dirname, '../packages'),
-    output: (name, version, type) => {
-      return path.resolve(
+    packages: [
+      {
+        name: '@pie-element/shared-config',
         // eslint-disable-next-line no-undef
-        __dirname,
-        '../packages',
-        name.replace('@pie-element/', ''),
-        'module',
-        `${type}.js`
-      );
-    },
-    extensions: { commonJs },
-  },
-  libs: [
-    {
-      name: '@pie-element/shared-config',
-      // eslint-disable-next-line no-undef
-      output: path.resolve(__dirname, '../packages'),
-      repository: 'pie-framework/pie-elements',
-      modules: [
-        /** make use of the pie-ui shared lib */
-        { name: '@pie-ui/shared-lib', version: '^2.5.0' },
-        { name: '@pie-ui/shared-math-edit', version: '^1.5.0' },
-      ],
-      /**
-       * Ideally namespace imports would be the default import method.
-       * But this can cause problems if a library does the following:
-       * `module.exports = require("path");` - this causes the properties to get lost,
-       * when really we'd like all the properties to be set on the module.
-       * For now - specify the import method as a key of `imports`.
-       *
-       * To fix this we'd probably need to make a change to rollup,
-       * get it to follow the var until it gets to the object definition,
-       * then to use the keys to set the export object.
-       *
-       * @see rollup/src/ast/NamespaceVariable
-       * @see rollup/plugin-commonjs/ too
-       */
-      imports: {
-        default: ['@pie-lib/editable-html'],
-        namespace: [
-          '@pie-framework/pie-configure-events',
-          '@pie-lib/config-ui',
+        output: path.resolve(__dirname, '../packages'),
+        repository: 'pie-framework/pie-elements',
+        modules: [
+          /** make use of the pie-ui shared lib */
+          { name: '@pie-ui/shared-lib', version: '^2.5.0' },
+          { name: '@pie-ui/shared-math-edit', version: '^1.5.0' },
         ],
+        /**
+         * Ideally namespace imports would be the default import method.
+         * But this can cause problems if a library does the following:
+         * `module.exports = require("path");` - this causes the properties to get lost,
+         * when really we'd like all the properties to be set on the module.
+         * For now - specify the import method as a key of `imports`.
+         *
+         * To fix this we'd probably need to make a change to rollup,
+         * get it to follow the var until it gets to the object definition,
+         * then to use the keys to set the export object.
+         *
+         * @see rollup/src/ast/NamespaceVariable
+         * @see rollup/plugin-commonjs/ too
+         */
+        imports: {
+          default: ['@pie-lib/editable-html'],
+          namespace: [
+            '@pie-framework/pie-configure-events',
+            '@pie-lib/config-ui',
+          ],
+        },
       },
-    },
-  ],
+    ],
+  },
 };
-
-/**
- * building element:
- *   open pkg.json, open pie
- *   if element - then we just need to point to the module url so : @pie-ui/mc@^4.11.12/module/index.js in a js file?
- *   if configure/controller - same?
- *
- * building configure:
- *   say cfg depends on pie ui - we want to mark that as an extrnal, then redirect it to the remote url
- *   also use the libs from any pie-ui el..
- *   - create any additional libs here
- *
- * building controller:
- * this is a node compatible + bm compatibale el - so should be a straight build (cant do a bm dependency) - so straight up
- *
- * (deployment): on npm publish - set up a hook that will pull the controller into a cf or lambda
- *
- * cmd usage:
- *   yarn pslb -c pslb.config.js
- *
- */
