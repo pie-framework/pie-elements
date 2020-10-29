55fb4692c95c50a60dbb4451218259db3239532a
ed eustace <ed.eustace@gmail.com>
2020-10-29 13:02:40 +0000

    fix: controller require

--- packages/inline-dropdown/controller/src/index.js
+++ packages/inline-dropdown/controller/src/index.js
@@ -33,7 +33,6 @@ export const normalize = (question) => ({
  * @param {*} updateSession - optional - a function that will set the properties passed into it on the session.
  */
 export function model(question, session, env, updateSession) {
-  console.log(require('@pie-lib/controller-utils'));
   return new Promise(async (resolve) => {
     const normalizedQuestion = normalize(question);
     const { value = {} } = session || {};
