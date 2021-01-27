0247e1c6e7c2894245b5141ad8634c865fdb746c
Elod Szopos <elodszopos@gmail.com>
2021-01-27 17:34:19 +0200

    chore: test fixes

--- packages/explicit-constructed-response/docs/demo/generate.js
+++ packages/explicit-constructed-response/docs/demo/generate.js
@@ -1,6 +1,7 @@
 const choice = (l, v) => ({ label: l, value: v });
 
-const markup = '<p>The {{0}} <br> jumped {{1}} <br> the {{2}}</p>';
+// const markup = '<p>The {{0}} <br> jumped {{1}} <br> the {{2}}</p>';
+const markup = `Let <math><mi>&#402;</mi><mo>(</mo><mi>x</mi><mo>)</mo><mo>=</mo><mfenced open="|" close="|"><mi>x</mi></mfenced><mo>+</mo><mn>5</mn></math>&#160;and <math><mi>g</mi><mo>(</mo><mi>x</mi><mo>)</mo><mo>=</mo><msup><mi>x</mi><mn>3</mn></msup><mo>-</mo><mn>2</mn><mi>x</mi><mo>+</mo><mn>3</mn></math>.<br>`;
 
 exports.model = (id, element) => ({
   id,
