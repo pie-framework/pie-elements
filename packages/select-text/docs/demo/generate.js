d73dab5ead8d6cf7cf40084f8a47f5567ee68634
ed eustace <ed.eustace@gmail.com>
2021-03-03 09:54:29 +0000

    fix: syntax error in generate

--- packages/select-text/docs/demo/generate.js
+++ packages/select-text/docs/demo/generate.js
@@ -3,19 +3,19 @@ const tokens = () => [
     text: 'Rachel cut out 8 stars in 6 minutes.',
     start: 0,
     end: 36,
-    correct: true
+    correct: true,
   },
   {
     text: 'Lovelle cut out 6 stars in 4 minutes.',
     start: 37,
     end: 74,
-    correct: true
+    correct: true,
   },
   {
     text: 'Lovelle and Rachel cut the same number of stars in 6 minutes.',
     start: 117,
-    end: 177
-  }
+    end: 177,
+  },
 ];
 
 const mathSampleText =
@@ -25,13 +25,16 @@ const mathSampleTokens = [
   {
     start: 583,
     end: 1465,
-    text: '<span class="lrn_token"><math xmlns="http://www.w3.org/1998/Math/MathML">\n  <mstyle displaystyle="true">\n    <mrow>\n      <mo>[</mo>\n      <mn>6</mn>\n      <mi>x</mi>\n      <mrow>\n        <mo>(</mo>\n        <mo>-</mo>\n        <msup>\n          <mi>x</mi>\n          <mn>2</mn>\n        </msup>\n        <mo>+</mo>\n        <mn>3</mn>\n        <mi>x</mi>\n        <mo>-</mo>\n        <mn>1</mn>\n        <mo>)</mo>\n      </mrow>\n      <mo>+</mo>\n      <mn>2</mn>\n      <mrow>\n        <mo>(</mo>\n        <mo>-</mo>\n        <msup>\n          <mi>x</mi>\n          <mn>2</mn>\n        </msup>\n        <mo>+</mo>\n        <mn>3</mn>\n        <mi>x</mi>\n        <mo>-</mo>\n        <mn>1</mn>\n        <mo>)</mo>\n      </mrow>\n      <mo>]</mo>\n    </mrow>\n    <mrow>\n      <mo>(</mo>\n      <msup>\n        <mi>x</mi>\n        <mn>2</mn>\n      </msup>\n      <mo>)</mo>\n    </mrow>\n  </mstyle>\n</math></span>', correct: true },
+    text:
+      '<span class="lrn_token"><math xmlns="http://www.w3.org/1998/Math/MathML">\n  <mstyle displaystyle="true">\n    <mrow>\n      <mo>[</mo>\n      <mn>6</mn>\n      <mi>x</mi>\n      <mrow>\n        <mo>(</mo>\n        <mo>-</mo>\n        <msup>\n          <mi>x</mi>\n          <mn>2</mn>\n        </msup>\n        <mo>+</mo>\n        <mn>3</mn>\n        <mi>x</mi>\n        <mo>-</mo>\n        <mn>1</mn>\n        <mo>)</mo>\n      </mrow>\n      <mo>+</mo>\n      <mn>2</mn>\n      <mrow>\n        <mo>(</mo>\n        <mo>-</mo>\n        <msup>\n          <mi>x</mi>\n          <mn>2</mn>\n        </msup>\n        <mo>+</mo>\n        <mn>3</mn>\n        <mi>x</mi>\n        <mo>-</mo>\n        <mn>1</mn>\n        <mo>)</mo>\n      </mrow>\n      <mo>]</mo>\n    </mrow>\n    <mrow>\n      <mo>(</mo>\n      <msup>\n        <mi>x</mi>\n        <mn>2</mn>\n      </msup>\n      <mo>)</mo>\n    </mrow>\n  </mstyle>\n</math></span>',
+    correct: true,
+  },
   { start: 1474, end: 2218 },
   { start: 2227, end: 2797 },
-  { start: 2806, end: 3112 }
+  { start: 2806, end: 3112 },
 ];
 
-const base = extras =>
+const base = (extras) =>
   Object.assign(
     {},
     {
@@ -39,16 +42,16 @@ const base = extras =>
       feedback: {
         correct: {
           type: 'default',
-          default: 'Correct'
+          default: 'Correct',
         },
         incorrect: {
           type: 'default',
-          default: 'Incorrect'
+          default: 'Incorrect',
         },
         partial: {
           type: 'default',
-          default: 'Nearly'
-        }
+          default: 'Nearly',
+        },
       },
       partialScoring: false,
       maxSelections: 2,
@@ -63,7 +66,7 @@ const base = extras =>
     extras
   );
 
-const mathSample = extras => {
+const mathSample = (extras) => {
   return Object.assign(
     {},
     {
@@ -71,21 +74,21 @@ const mathSample = extras => {
       feedback: {
         correct: {
           type: 'default',
-          default: 'Correct'
+          default: 'Correct',
         },
         incorrect: {
           type: 'default',
-          default: 'Incorrect'
+          default: 'Incorrect',
         },
         partial: {
           type: 'default',
-          default: 'Nearly'
-        }
+          default: 'Nearly',
+        },
       },
       partialScoring: false,
       maxSelections: 2,
       prompt: 'What sentences contain the character 6 in them?',
-      text:mathSampleText
+      text: mathSampleText,
       tokens: mathSampleTokens,
     },
     extras
@@ -101,71 +104,84 @@ exports.mathSample = (id, element) => {
 };
 
 exports.htmlSample = (id, element) => {
-  return Object.assign({}, { id, element },     {
-    highlightChoices: true,
-    feedback: {
-      correct: {
-        type: 'default',
-        default: 'Correct'
+  return Object.assign(
+    {},
+    { id, element },
+    {
+      highlightChoices: true,
+      feedback: {
+        correct: {
+          type: 'default',
+          default: 'Correct',
+        },
+        incorrect: {
+          type: 'default',
+          default: 'Incorrect',
+        },
+        partial: {
+          type: 'default',
+          default: 'Nearly',
+        },
       },
-      incorrect: {
-        type: 'default',
-        default: 'Incorrect'
-      },
-      partial: {
-        type: 'default',
-        default: 'Nearly'
-      }
-    },
-    partialScoring: false,
-    text: '<p style=\'padding: 20px\'>A flicker and then gone. In its wake, only <span class="lrn_token">the after-echo of a distant high-pitched ringing that vibrated through him even as it returned to silence once again</span>, as he stared into the smiling faces of the crew and told himself that yes he was meant to be here, that <span class="lrn_token">it had been his destiny to succeed and so he had succeeded</span>, the sense of panic already replaced by the pride of his accomplishment, replaced so completely that he would not remember that flicker, would not allow himself to remember, <span class="lrn_token">even after everything else had happened</span>. In his memory of that moment, <span class="lrn_token">he was an astronaut and they were welcoming him aboard the space station</span>. And in his memory he was smiling. Is this not what he was meant to do? Is the answer not as fixed and indisputable as any equation he might have been tasked to solve?</p>\n',
-    prompt: '<p>Select the <span class="relative-emphasis">one</span> phrase below that suggests that in the future, Keith may interpret this moment differently.</p>',
-    maxSelections: 2,
-    mode: 'sentence',
-    rationale: 'Rationale goes here.',
-    tokens: [
-      {
-        correct: false,
-        start: 67,
-        end: 215,
-        text: '<span class="lrn_token">the after-echo of a distant high-pitched ringing that vibrated through him even as it returned to silence once again</span>'
-      },
-      {
-        correct: false,
-        start: 321,
-        end: 410,
-        text: '<span class="lrn_token">it had been his destiny to succeed and so he had succeeded</span>'
-      },
-      {
-        correct: true,
-        start: 585,
-        end: 655,
-        text: '<span class="lrn_token">even after everything else had happened</span>'
-      },
-      {
-        correct: false,
-        start: 687,
-        end: 790,
-        text: '<span class="lrn_token">he was an astronaut and they were welcoming him aboard the space station</span>'
-      }
-    ],
-  });
+      partialScoring: false,
+      text:
+        '<p style=\'padding: 20px\'>A flicker and then gone. In its wake, only <span class="lrn_token">the after-echo of a distant high-pitched ringing that vibrated through him even as it returned to silence once again</span>, as he stared into the smiling faces of the crew and told himself that yes he was meant to be here, that <span class="lrn_token">it had been his destiny to succeed and so he had succeeded</span>, the sense of panic already replaced by the pride of his accomplishment, replaced so completely that he would not remember that flicker, would not allow himself to remember, <span class="lrn_token">even after everything else had happened</span>. In his memory of that moment, <span class="lrn_token">he was an astronaut and they were welcoming him aboard the space station</span>. And in his memory he was smiling. Is this not what he was meant to do? Is the answer not as fixed and indisputable as any equation he might have been tasked to solve?</p>\n',
+      prompt:
+        '<p>Select the <span class="relative-emphasis">one</span> phrase below that suggests that in the future, Keith may interpret this moment differently.</p>',
+      maxSelections: 2,
+      mode: 'sentence',
+      rationale: 'Rationale goes here.',
+      tokens: [
+        {
+          correct: false,
+          start: 67,
+          end: 215,
+          text:
+            '<span class="lrn_token">the after-echo of a distant high-pitched ringing that vibrated through him even as it returned to silence once again</span>',
+        },
+        {
+          correct: false,
+          start: 321,
+          end: 410,
+          text:
+            '<span class="lrn_token">it had been his destiny to succeed and so he had succeeded</span>',
+        },
+        {
+          correct: true,
+          start: 585,
+          end: 655,
+          text:
+            '<span class="lrn_token">even after everything else had happened</span>',
+        },
+        {
+          correct: false,
+          start: 687,
+          end: 790,
+          text:
+            '<span class="lrn_token">he was an astronaut and they were welcoming him aboard the space station</span>',
+        },
+      ],
+    }
+  );
 };
 
 exports.htmlAscii = (id, element) => ({
-    id,
-    element,
-    highlightChoices: true,
-    prompt: 'Select the parts that have references for Lucy',
-    text: '<p>&#8220;Lucy? Are you using your time wisely to plan your project?&#8221; Mr. Wilson asked.</p><p>&nbsp;Lucy looked a little confused at first.</p>',
-    tokens: [{
+  id,
+  element,
+  highlightChoices: true,
+  prompt: 'Select the parts that have references for Lucy',
+  text:
+    '<p>&#8220;Lucy? Are you using your time wisely to plan your project?&#8221; Mr. Wilson asked.</p><p>&nbsp;Lucy looked a little confused at first.</p>',
+  tokens: [
+    {
       start: 10,
       end: 68,
-      text: 'Lucy? Are you using your time wisely to plan your project?'
-    }, {
+      text: 'Lucy? Are you using your time wisely to plan your project?',
+    },
+    {
       start: 106,
       end: 145,
-      text: 'Lucy looked a little confused at first.'
-    }]
-  }
-);
+      text: 'Lucy looked a little confused at first.',
+    },
+  ],
+});
