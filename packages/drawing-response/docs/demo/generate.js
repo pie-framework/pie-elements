1dd1ec715cbaf9a0f3c73c0ea454a0f275c2e85c
Diana Gociu <diana.gociu@yahoo.com>
2021-07-12 11:56:05 +0300

    feat(drawing-response) : added option for toolbar editor position

--- packages/drawing-response/docs/demo/generate.js
+++ packages/drawing-response/docs/demo/generate.js
@@ -3,6 +3,7 @@ exports.model = (id, element) => ({
   element,
   prompt: 'This is the question prompt',
   promptEnabled: true,
+  toolbarEditorPosition: 'top',
   imageUrl: '',
   imageDimensions: {
     height: 0,
