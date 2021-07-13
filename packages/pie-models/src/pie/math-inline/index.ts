01282e4a07d200f7b81c684a11b51aa1f226ddeb
Diana Gociu <diana.gociu@yahoo.com>
2021-07-13 15:26:20 +0300

    feat(math-inline): added option to set toolbar editor position

--- packages/pie-models/src/pie/math-inline/index.ts
+++ packages/pie-models/src/pie/math-inline/index.ts
@@ -113,6 +113,12 @@ export interface MathInlinePie extends PieModel {
 
     /** Indicates if Teacher Instructions are enabled */
     teacherInstructionsEnabled: boolean;
+
+    /**
+     * Indicates the editor's toolbar position which can be 'bottom' or 'top'
+     * @default: 'bottom'
+     */
+    toolbarEditorPosition?: 'bottom' | 'top';
 }
 
 
