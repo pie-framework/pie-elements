5faaac138f9fd9c029573aafc77cb236a228d8fd
ed eustace <ed.eustace@gmail.com>
2021-08-10 12:44:11 +0100

    print sample build

---
+++ packages/multiple-choice/src/print.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import debounce from 'lodash/debounce';
+import Main from './main';
+import { renderMath } from '@pie-lib/math-rendering';
+import debug from 'debug';
+
+const log = debug('pie-element:multiple-choice:print');
+
+/**
+ * Live in same package as main element - so we can access some of the shared comps!
+ *
+ * - update pslb to build print if src/print.js is there
+ * - update demo el
+ * - get configure/controller building
+ */
+export default class MultipleChoicePrint extends HTMLElement {
+  constructor() {
+    super();
+    this._options = null;
+    this._model = null;
+    this._session = [];
+    this._rerender = debounce(
+      () => {
+        if (this._model && this._session) {
+          var element = React.createElement(Main, {
+            model: this._model,
+            session: {},
+          });
+          ReactDOM.render(element, this, () => {
+            log('render complete - render math');
+            renderMath(this);
+          });
+        } else {
+          log('skip');
+        }
+      },
+      50,
+      { leading: false, trailing: true }
+    );
+  }
+  set options(o) {
+    this._options = o;
+  }
+
+  set model(s) {
+    this._model = s;
+    this._rerender();
+  }
+
+  connectedCallback() {
+    log('connected...');
+    // this.innerHTML = '<h1>PRINT</h1>';
+  }
+}
