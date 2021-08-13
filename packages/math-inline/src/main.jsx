96db6541cb95ce895be7d84a4763697880e35adf
Andrei Miron <andrei.miron@mcro-e.com>
2021-08-13 12:33:05 +0300

    fix(math-inline): Added a min width for line notation PD-1263

--- packages/math-inline/src/main.jsx
+++ packages/math-inline/src/main.jsx
@@ -614,6 +614,9 @@ const styles = (theme) => ({
     },
     '& .mq-overarrow-inner-left': {
       display: 'none !important'
+    },
+    '& .mq-overarrow.mq-arrow-both': {
+      minWidth: '1.23em'
     }
   },
   showCorrectness: {
