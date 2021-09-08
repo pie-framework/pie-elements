c3f5ea2511cf73b769e5955923649c4cd9a503b0
ed eustace <ed.eustace@gmail.com>
2021-09-08 14:01:45 +0100

    chore: bump all deps

--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -1,120 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
 const _ = require('lodash');
-const reactIsExports = [
-  'AsyncMode',
-  'ConcurrentMode',
-  'ContextConsumer',
-  'ContextProvider',
-  'Element',
-  'ForwardRef',
-  'Fragment',
-  'Lazy',
-  'Memo',
-  'Portal',
-  'Profiler',
-  'StrictMode',
-  'Suspense',
-  'isAsyncMode',
-  'isConcurrentMode',
-  'isContextConsumer',
-  'isContextProvider',
-  'isElement',
-  'isForwardRef',
-  'isFragment',
-  'isLazy',
-  'isMemo',
-  'isPortal',
-  'isProfiler',
-  'isStrictMode',
-  'isSuspense',
-  'isValidElementType',
-  'typeOf',
-];
-
-const konva = [
-  'Layer',
-  'FastLayer',
-  'Group',
-  'Label',
-  'Rect',
-  'Circle',
-  'Ellipse',
-  'Wedge',
-  'Transformer',
-  'Line',
-  'Sprite',
-  'Image',
-  'Text',
-  'TextPath',
-  'Star',
-  'Ring',
-  'Arc',
-  'Tag',
-  'Path',
-  'RegularPolygon',
-  'Arrow',
-  'Shape',
-  'useStrictMode',
-  'Stage',
-];
-
-const immutable = [
-  'Map',
-  'Set',
-  'List',
-  'Iterable',
-  'Seq',
-  'Collection',
-  'OrderedMap',
-  'Stack',
-  'OrderedSet',
-  'Record',
-  'Range',
-  'Repeat',
-  'is',
-  'fromJS',
-];
-
-const commonJs = {
-  namedExports: {
-    'node_modules/react-beautiful-dnd/node_modules/react-is/index.js': [
-      'isValidElementType',
-      'isContextConsumer',
-    ],
-    'node_modules/js-combinatorics/combinatorics.js': ['combination'],
-    'node_modules/react-konva/lib/ReactKonva.js': konva,
-    'node_modules/react-redux/node_modules/react-is/index.js': reactIsExports,
-    //TODO: common js should be picking these up?
-    'node_modules/react-is/index.js': reactIsExports,
-    'node_modules/react/index.js': [
-      'memo',
-      'useLayoutEffect',
-      'useEffect',
-      'useState',
-      'useCallback',
-      'useContext',
-      'cloneElement',
-      'createElement',
-      'createContext',
-      'isValidElement',
-      'useMemo',
-      'useRef',
-      'createRef',
-      'Component',
-      'useReducer',
-    ],
-    'node_modules/humps/humps.js': ['camelizeKeys'],
-    'node_modules/react-dom/server.browser.js': ['renderToStaticMarkup'],
-    'node_modules/react-dom/index.js': [
-      'findDOMNode',
-      'unstable_batchedUpdates',
-    ],
-    'node_modules/esrever/esrever.js': ['reverse'],
-    'node_modules/slate-plain-serializer/node_modules/immutable/dist/immutable.js': immutable,
-    'node_modules/immutable/dist/immutable.js': immutable,
-  },
-};
 
 const blacklist = [
   'pie-models',
@@ -122,6 +8,7 @@ const blacklist = [
   'protractor',
   'ruler',
   'calculator',
+  'select-text',
 ];
 
 const packagesDir = path.resolve(__dirname, '../packages');
@@ -136,9 +23,7 @@ const listPackages = () => {
     files
       .filter((f) => !f.includes('@'))
 
-      // Note: we only build multiple-choice for now
-      .filter((f) => f.includes('multiple-choice'))
-      //!blacklist.includes(f))
+      .filter((f) => !blacklist.includes(f))
       .map((f) => {
         try {
           const rootPkg = fs.readJsonSync(
@@ -162,21 +47,13 @@ module.exports = {
     element: true,
   },
   extensions: {
-    commonJs,
+    commonJs: {},
   },
   mode: 'development',
   range: '^',
   minify: false,
   libs: {
     repository: 'pie-framework/pie-elements',
-    /**
-     * ["@pie-lib/drag-module@1.0.1",
-     * "@pie-lib/math-rendering-module@1.0.3",
-     * "@pie-lib/math-edit-module@1.0.1",
-     * "@pie-lib/shared-module@1.1.0",
-     * "@pie-lib/editable-html-module@1.0.1",
-     * "@pie-lib/config-module@1.0.1"]
-     */
     packages: [
       { name: '@pie-lib/drag-module', version: '^1.0.0' },
       { name: '@pie-lib/math-rendering-module', version: '^1.0.0' },
@@ -184,7 +61,6 @@ module.exports = {
       { name: '@pie-lib/shared-module', version: '^1.1.0' },
       { name: '@pie-lib/editable-html-module', version: '^1.0.0' },
       { name: '@pie-lib/config-module', version: '^1.0.0' },
-      // { name: '@pie-lib/graphing-module', version: '^1.0.0' },
     ],
   },
 };
