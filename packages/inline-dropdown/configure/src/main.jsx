477066b51d746d37ba8ba81ece42c95972466fc3
Andrei Miron <andrei.miron@mcro-e.com>
2022-04-12 15:37:58 +0300

    feat(inline-dropdown): added validation UI improvements PD-1698

--- packages/inline-dropdown/configure/src/main.jsx
+++ packages/inline-dropdown/configure/src/main.jsx
@@ -9,18 +9,20 @@ import isEqual from 'lodash/isEqual';
 import isUndefined from 'lodash/isUndefined';
 import isEmpty from 'lodash/isEmpty';
 import reduce from 'lodash/reduce';
+import max from 'lodash/max';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Info from '@material-ui/icons/Info';
 import InlineDropdownToolbar from './inline-dropdown-toolbar';
-import max from 'lodash/max';
 
 const { toggle, Panel } = settings;
 
@@ -85,7 +87,8 @@ const styles = theme => ({
     fontFamily: 'Cerebri Sans',
     fontSize: '16px',
     lineHeight: '19px',
-    color: '#495B8F'
+    color: '#495B8F',
+    marginRight: '5px'
   },
   rationaleLabel: {
     display: 'flex',
@@ -96,6 +99,16 @@ const styles = theme => ({
   },
   panelDetails: {
    display: 'block'
+  },
+  flexContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '5px'
+  },
+  tooltip: {
+    fontSize: '12px',
+    whiteSpace: 'pre',
+    maxWidth: '500px'
   },
   errorText: {
     fontSize: '12px',
@@ -384,6 +397,7 @@ export class Main extends React.Component {
       choiceRationale = {},
       teacherInstructions = {},
       spellCheck = {},
+      maxResponseAreas
     } = configuration || {};
     const {
       rationaleEnabled,
@@ -393,8 +407,7 @@ export class Main extends React.Component {
       choices,
       spellCheckEnabled,
       errors
-    } =
-    model || {};
+    } = model || {};
     const { responseAreasError } = errors || {};
 
     const renderChoiceRationale = () => (Object.keys(choices) || []).map(key =>
@@ -449,6 +462,11 @@ export class Main extends React.Component {
         toolbarOpts.position = 'bottom';
         break;
     }
+
+    const validationMessage = 'Validation rules:\n' +
+      'There should be at least 1' +
+      (maxResponseAreas ? ` and at most ${maxResponseAreas}` : '') + ' response area' +
+      (maxResponseAreas ? 's.' : '.');
 
     return (
       <div className={classes.design}>
@@ -536,10 +554,22 @@ export class Main extends React.Component {
               </InputContainer>
             )}
 
-            <Typography className={classes.text}>
-              Define Template, Choices, and Correct Responses
-            </Typography>
+            <div className={classes.flexContainer}>
+              <Typography className={classes.text}>
+                Define Template, Choices, and Correct Responses
+              </Typography>
+              <Tooltip
+                classes={{ tooltip: classes.tooltip }}
+                disableFocusListener
+                disableTouchListener
+                placement={'right'}
+                title={validationMessage}
+              >
+                <Info fontSize={'small'} color={'primary'}/>
+              </Tooltip>
+            </div>
             {responseAreasError && <div className={classes.errorText}>{responseAreasError}</div>}
+
             <InfoDialog
               open={dialog.open}
               title={dialog.message}
@@ -554,6 +584,7 @@ export class Main extends React.Component {
                 options: {
                   duplicates: true
                 },
+                maxResponseAreas: maxResponseAreas,
                 respAreaToolbar: (node, value, onToolbarDone) => {
                   const { respAreaChoices } = this.state;
 
