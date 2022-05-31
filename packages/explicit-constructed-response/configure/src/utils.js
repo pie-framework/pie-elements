dff49645a4b814686bfaba18298b064e174c717a
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2022-05-31 14:35:17 +0300

    fix(explicit-constructed-response): change validation messsage

--- packages/explicit-constructed-response/configure/src/utils.js
+++ packages/explicit-constructed-response/configure/src/utils.js
@@ -1,8 +1,9 @@
 export const generateValidationMessage = config => {
   const { maxResponseAreas } = config;
 
-  const responseAreasMessage = '\nThe response area content should ' +
-    'not be empty and should be unique.\nThere should be at least 1 ' +
+  const responseAreasMessage = '\nCorrect answers should not be blank.' +
+    '\nEach answer defined for a response area should be unique.' +
+    '\nThere should be at least 1 ' +
     (maxResponseAreas ? `and at most ${maxResponseAreas} ` : '') +
     'response area' + (maxResponseAreas ? 's' : '') + ' defined.';
 
