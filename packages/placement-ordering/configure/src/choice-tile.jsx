bd526c8935c539ecf1d65d9aabc367f52a6b6b79
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2022-11-11 14:28:44 +0200

    fix(placement-ordering): move the delete choice button PD-2170

--- packages/placement-ordering/configure/src/choice-tile.jsx
+++ packages/placement-ordering/configure/src/choice-tile.jsx
@@ -93,41 +93,40 @@ export class ChoiceTile extends React.Component {
     const markup = (
       <div className={classes.choiceTile} style={{ opacity: opacity, width: '100%' }}>
         <CardActions>
-            <span className={classNames(classes.dragHandle )}>
-              <DragHandle className={classes.actions} />
+            <span className={classNames(classes.dragHandle)}>
+              <DragHandle className={classes.actions}/>
             </span>
         </CardActions>
-        <div style={{width: '100%'}}>
-        <EditableHtml
-          disabled={!editable}
-          className={classNames(classes.prompt, !editable && classes.targetPrompt)}
-          placeholder="Enter a choice"
-          markup={label}
-          imageSupport={imageSupport || undefined}
-          onChange={this.onLabelChange}
-          pluginProps={choicePlugins}
-          toolbarOpts={toolbarOpts}
-          activePlugins={filteredDefaultPlugins}
-          spellCheck={spellCheck}
-          allowValidation
-          maxImageWidth={maxImageWidth}
-          maxImageHeight={maxImageHeight}
-          languageCharactersProps={[{ language: 'spanish' }, { language: 'special' }]}
-          error={editable && error}
-        />
-        {editable && error && <div className={classes.errorText}>{error}</div>}
+        <div style={{ width: '100%', display: 'flex' }}>
+          <EditableHtml
+            disabled={!editable}
+            className={classNames(classes.prompt, !editable && classes.targetPrompt)}
+            placeholder="Enter a choice"
+            markup={label}
+            imageSupport={imageSupport || undefined}
+            onChange={this.onLabelChange}
+            pluginProps={choicePlugins}
+            toolbarOpts={toolbarOpts}
+            activePlugins={filteredDefaultPlugins}
+            spellCheck={spellCheck}
+            maxImageWidth={maxImageWidth}
+            maxImageHeight={maxImageHeight}
+            languageCharactersProps={[{ language: 'spanish' }, { language: 'special' }]}
+            error={editable && error}
+          />
+          {editable && error && <div className={classes.errorText}>{error}</div>}
+          {editable && (
+            <div className={classes.controls}>
+              <IconButton color='default' onClick={onDelete}>
+                <RemoveCircle
+                  classes={{
+                    root: classes.removeCircle
+                  }}
+                />
+              </IconButton>
+            </div>
+          )}
         </div>
-        {editable && (
-          <div className={classes.controls}>
-            <IconButton color='default' onClick={onDelete}>
-              <RemoveCircle
-                classes={{
-                  root: classes.removeCircle
-                }}
-              />
-            </IconButton>
-          </div>
-        )}
         <InfoDialog
           title={dialog.message}
           open={dialog.open}
