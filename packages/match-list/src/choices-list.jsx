26aabde6c6427b435d80c607cbb66436d6b89810
Carla Costea <carlacostea@yahoo.com>
2022-12-01 10:01:52 +0200

    refactor and cleanup, remove droppable-placeholder from pie-elements

--- packages/match-list/src/choices-list.jsx
+++ packages/match-list/src/choices-list.jsx
@@ -5,7 +5,7 @@ import isEmpty from 'lodash/isEmpty';
 import isUndefined from 'lodash/isUndefined';
 import find from 'lodash/find';
 import { DragAnswer } from './answer';
-import PlaceHolder from './droppable-placeholder';
+import { DroppablePlaceholder } from '@pie-lib/drag';
 
 export class ChoicesList extends React.Component {
   static propTypes = {
@@ -13,60 +13,47 @@ export class ChoicesList extends React.Component {
     session: PropTypes.object.isRequired,
     instanceId: PropTypes.string.isRequired,
     model: PropTypes.object.isRequired,
-    disabled: PropTypes.bool.isRequired
+    disabled: PropTypes.bool.isRequired,
   };
 
   render() {
-    const {
-      model,
-      classes,
-      disabled,
-      session,
-      instanceId,
-      onDropChoice,
-    } = this.props;
+    const { model, classes, disabled, session, instanceId } = this.props;
     const { config } = model;
     const { duplicates } = config;
 
     return (
-      <div className={classes.answersContainer}>
-        <PlaceHolder disabled={disabled} onDropChoice={onDropChoice}>
-        {
-          config.answers
-            .filter(answer => (duplicates || isEmpty(session) || !session.value || isUndefined(find(session.value, val => val === answer.id))))
-            .map((answer) => (
-              <DragAnswer
-                key={answer.id}
-                instanceId={instanceId}
-                className={classes.choice}
-                draggable={true}
-                disabled={disabled}
-                session={session}
-                type={'choice'}
-                {...answer}
-              />
-            ))
-        }
-        </PlaceHolder>
-      </div>
+      <DroppablePlaceholder disabled={disabled} choiceBoard={true}>
+        {config.answers
+          .filter(
+            (answer) =>
+              duplicates ||
+              isEmpty(session) ||
+              !session.value ||
+              isUndefined(find(session.value, (val) => val === answer.id))
+          )
+          .map((answer) => (
+            <DragAnswer
+              key={answer.id}
+              instanceId={instanceId}
+              className={classes.choice}
+              draggable={true}
+              disabled={disabled}
+              session={session}
+              type={'choice'}
+              {...answer}
+            />
+          ))}
+      </DroppablePlaceholder>
     );
   }
 }
 
 const styles = () => ({
-  answersContainer: {
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    marginTop: 50
-  },
   choice: {
     minHeight: '40px',
     minWidth: '200px',
-    height: 'initial'
-  }
+    height: 'initial',
+  },
 });
 
 export default withStyles(styles)(ChoicesList);
