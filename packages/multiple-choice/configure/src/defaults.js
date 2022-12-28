a06aa138d155d8721dd4b5deb95adbad7501bf7f
Andrei Miron <andrei.miron@mcro-e.com>
2022-12-28 18:27:15 +0200

    added default value for settings panel

--- packages/multiple-choice/configure/src/defaults.js
+++ packages/multiple-choice/configure/src/defaults.js
@@ -73,6 +73,7 @@ export default {
       settings: false,
       label: 'Accessibility Label',
     },
+    settingsPanelDisabled: false,
     scoringType: {
       settings: false,
       label: 'Scoring Type',
