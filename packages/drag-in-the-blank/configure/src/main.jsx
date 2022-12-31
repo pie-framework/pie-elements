630cc87640183a21e456876b6389d2694a0e5553
Andrei Miron <andrei.miron@mcro-e.com>
2022-12-31 12:16:26 +0200

    feat(drag-in-the-blank): added configuration prop to hide the settings panel PD-1999

--- packages/drag-in-the-blank/configure/src/main.jsx
+++ packages/drag-in-the-blank/configure/src/main.jsx
@@ -151,6 +151,7 @@ export class Main extends React.Component {
       teacherInstructions = {},
       choicesPosition = {},
       spellCheck = {},
+      settingsPanelDisabled,
       maxChoices,
       maxResponseAreas,
       maxImageWidth = {},
@@ -175,9 +176,26 @@ export class Main extends React.Component {
         toolbarOpts.position = 'bottom';
         break;
     }
+
+    const panelSettings = {
+      partialScoring: partialScoring.settings && toggle(partialScoring.label),
+      duplicates: duplicates.settings && toggle(duplicates.label),
+      lockChoiceOrder: lockChoiceOrder.settings && toggle(lockChoiceOrder.label),
+      choicesPosition: choicesPosition.settings && dropdown(choicesPosition.label, ['above', 'below', 'left', 'right']),
+    };
+
+    const panelProperties = {
+      teacherInstructionsEnabled: teacherInstructions.settings && toggle(teacherInstructions.label),
+      rationaleEnabled: rationale.settings && toggle(rationale.label),
+      spellCheckEnabled: spellCheck.settings && toggle(spellCheck.label),
+      promptEnabled: prompt.settings && toggle(prompt.label),
+      rubricEnabled: withRubric?.settings && toggle(withRubric?.label),
+    };
+
     return (
       <div className={classes.design}>
         <layout.ConfigLayout
+          hideSettings={settingsPanelDisabled}
           settings={
             <Panel
               model={model}
@@ -185,20 +203,8 @@ export class Main extends React.Component {
               onChangeModel={(model) => this.onModelChange(model)}
               onChangeConfiguration={(configuration) => onConfigurationChanged(configuration, true)}
               groups={{
-                Settings: {
-                  partialScoring: partialScoring.settings && toggle(partialScoring.label),
-                  duplicates: duplicates.settings && toggle(duplicates.label),
-                  lockChoiceOrder: lockChoiceOrder.settings && toggle(lockChoiceOrder.label),
-                  choicesPosition:
-                    choicesPosition.settings && dropdown(choicesPosition.label, ['above', 'below', 'left', 'right']),
-                },
-                Properties: {
-                  teacherInstructionsEnabled: teacherInstructions.settings && toggle(teacherInstructions.label),
-                  rationaleEnabled: rationale.settings && toggle(rationale.label),
-                  spellCheckEnabled: spellCheck.settings && toggle(spellCheck.label),
-                  promptEnabled: prompt.settings && toggle(prompt.label),
-                  rubricEnabled: withRubric?.settings && toggle(withRubric?.label),
-                },
+                Settings: panelSettings,
+                Properties: panelProperties,
               }}
             />
           }
