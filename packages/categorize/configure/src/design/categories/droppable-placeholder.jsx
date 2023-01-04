b124162ccfcccd0600ca0fcee1431796cc5a7b53
arimieandreea <arimie_andreea@yahoo.com>
2023-01-04 14:19:07 +0200

    fix(categorize): transform to else if Pd-1967

--- packages/categorize/configure/src/design/categories/droppable-placeholder.jsx
+++ packages/categorize/configure/src/design/categories/droppable-placeholder.jsx
@@ -103,11 +103,9 @@ export const spec = {
 
     if (item.from && item.alternateResponseIndex === props.alternateResponseIndex) {
       props.onMoveChoice(item.choiceId, item.from, props.categoryId, item.choiceIndex, item.alternateResponseIndex);
-    } else{
+    } else if (!item.from){
       // avoid dropping choice when user tries to move it to an alternate with other index
-      if(!item.from){
-        props.onDropChoice(item, props.categoryId);
-      }
+      props.onDropChoice(item, props.categoryId);
     }
   },
   canDrop: (props /*, monitor*/) => {
