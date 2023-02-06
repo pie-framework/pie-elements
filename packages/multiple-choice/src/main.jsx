aaeec5cc6f1ead5eedf6fd2d8326d378aa5b3968
tiberiu@vteeam.com <carlacostea@yahoo.com>
2023-02-06 14:19:44 +0200

    fix(multiple-choice): add aria-label and role attributes on parent element

--- packages/multiple-choice/src/main.jsx
+++ packages/multiple-choice/src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
@@ -22,15 +23,23 @@ class Main extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    const { model } = this.props;
+    const ariaLabel = model.choiceMode == 'radio' ? 'Multiple Choice Question' : 'Multiple Correct Answer Question';
+
+    const currentNode = ReactDOM.findDOMNode(this);
+    const parentNode = currentNode.parentNode;
+
+    parentNode.setAttribute('aria-label', ariaLabel);
+    parentNode.setAttribute('role', 'region');
+  }
+
   render() {
     const { model, onChoiceChanged, session, classes } = this.props;
 
     // model.partLabel is a property used for ebsr
     return (
-      <PreviewLayout
-        ariaLabel={(model.choiceMode == 'radio' ? 'Multiple Choice Question' : 'Multiple Correct Answer Question')}
-        role="region"
-      > 
+      <PreviewLayout>
         {model.partLabel && <p>{model.partLabel}</p>}
         <div className={classNames(classes.root, classes[model.className])}>
           <MultipleChoice {...model} session={session} onChoiceChanged={onChoiceChanged} />
