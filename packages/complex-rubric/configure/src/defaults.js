40b83ca677d464a218c4e7b9b2c2c2309e834e17
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2023-02-07 17:32:57 +0200

    comment width in defaults

--- packages/complex-rubric/configure/src/defaults.js
+++ packages/complex-rubric/configure/src/defaults.js
@@ -124,7 +124,7 @@ export default {
     rubrics: { simpleRubric: rubricDefaultModel, multiTraitRubric: multiTraitDefaultModel },
   },
   configuration: {
-    width: '770px',
+    // width: '770px',
     multiTraitRubric: multiTraitDefaultConfiguration,
     simpleRubric: rubricDefaultConfiguration,
   },
