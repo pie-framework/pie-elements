db285e529b1c581f48f505f76eee10d485e47b2f
Carla <carlacostea@yahoo.com>
2023-03-14 14:47:16 +0200

    cleanup

--- packages/multiple-choice/docs/demo/session.js
+++ packages/multiple-choice/docs/demo/session.js
@@ -2,11 +2,6 @@ module.exports = [
   {
     id: '1',
     element: 'multiple-choice',
-  
-      "value": [
-          "8a808081841c101e01843f614bd41179",
-          "8a808081841c101e01843f614bd31177"
-      ]
-
+    // value: ['iceland', 'norway']
   },
 ];
