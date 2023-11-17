773eece6242b1f3085602997847b9d24f4de9e56
carlacostea <carlacostea@yahoo.com>
2023-11-17 14:10:12 +0200

    fix(charting): Add dispatching of ModelSetEvent on model update in charting component, add tests PD-3270

--- packages/charting/src/index.js
+++ packages/charting/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
-import { SessionChangedEvent } from '@pie-framework/pie-player-events';
+import { SessionChangedEvent, ModelSetEvent } from '@pie-framework/pie-player-events';
 import { renderMath } from '@pie-lib/pie-toolbox/math-rendering';
 
 export { Main as Component };
