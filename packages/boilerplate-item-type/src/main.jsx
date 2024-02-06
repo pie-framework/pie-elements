86f22abb294059cb1202082aa8ed0dd971b6aecd
andreeapescar <andreeaflaviapescar@gmail.com>
2024-02-06 15:26:07 +0200

    chore: more experiments

--- packages/boilerplate-item-type/src/main.jsx
+++ packages/boilerplate-item-type/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PreviewPrompt } from '@pie-lib/pie-toolbox/render-ui';
-
+import TEST from '@pie-element/common';
 export default class Main extends React.Component {
   static propTypes = {
     model: PropTypes.object,
@@ -18,6 +18,7 @@ export default class Main extends React.Component {
       <div>
         {model.prompt && (
           <div>
+            {TEST}
             <p>Boilerplate Item</p>
             <PreviewPrompt prompt={model.prompt}/>
           </div>
