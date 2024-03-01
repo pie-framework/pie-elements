f69452e920300bd67cd714e298d8c0fc96cf40c4
Adrian Barta <adrian.barta@mcro.tech>
2024-03-01 14:52:30 +0200

    feat: add support for teacher instructions on passage PD-3445

--- packages/passage/controller/src/index.js
+++ packages/passage/controller/src/index.js
@@ -6,8 +6,9 @@
  * @param {*} updateSession - optional - a function that will set the properties passed into it on the session.
  */
 export async function model(question, session, env, updateSession) {
-  // TODO use env to send teacherInstructions
-  console.log({ question, env });
+  const { role, mode } = env || {};
+
+  question.showTeacherInstructions = !!(role === 'instructor' && (mode === 'view' || mode === 'evaluate'));
 
   return question;
 }
