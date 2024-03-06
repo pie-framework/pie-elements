a1a681a9f614dc54e57eb81aa56886cfaaa22310
Adrian Barta <adrian.barta@mcro.tech>
2024-03-06 13:06:00 +0200

    feat: Add configuration for custom buttons PD-3508

--- packages/matrix/configure/src/defaults.js
+++ packages/matrix/configure/src/defaults.js
@@ -6,15 +6,25 @@ export default {
   model: {
     teacherInstructionsEnabled: true,
     labelType: 'agreement',
-    rowLabels: ['I\'m interested in politics.', 'I\'m interested in economics.'],
+    rowLabels: ["I'm interested in politics.", "I'm interested in economics."],
     columnLabels: ['Disagree', 'Unsure', 'Agree'],
     matrixValues: {},
     prompt: 'How interested are you in the following domains?',
   },
   configuration: {
+    baseInputConfiguration: {
+      audio: { disabled: false },
+      video: { disabled: false },
+      image: { disabled: false },
+    },
     prompt: {
       settings: true,
       label: 'Prompt',
+      inputConfiguration: {
+        audio: { disabled: false },
+        video: { disabled: false },
+        image: { disabled: false },
+      },
     },
     settingsPanelDisabled: false,
     spellCheck: {
@@ -25,6 +35,11 @@ export default {
     teacherInstructions: {
       settings: true,
       label: 'Teacher Instructions',
+      inputConfiguration: {
+        audio: { disabled: false },
+        video: { disabled: false },
+        image: { disabled: false },
+      },
     },
   },
 };
