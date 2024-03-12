f5614eeca83dedc518e4aef4fabf1899bfbfca10
carlacostea <carlacostea@yahoo.com>
2024-03-12 12:17:31 +0200

    feat(multiple-choice): use math-rendering-accessible PD-1870

--- packages/multiple-choice/src/index.js
+++ packages/multiple-choice/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import debounce from 'lodash/debounce';
 import debug from 'debug';
-import { renderMath } from '@pie-lib/pie-toolbox/math-rendering';
+import { renderMath } from '@pie-lib/pie-toolbox/math-rendering-accessible';
 import { updateSessionValue } from './session-updater';
 
 const log = debug('pie-ui:multiple-choice');
