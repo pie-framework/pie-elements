0999aec9e82670e26625632bb601d970bb87ef56
Adrian Barta <adrian.barta@mcro.tech>
2024-05-08 13:50:32 +0300

    fix(extended-text-entry): add characters limit of 50000 PD-3709

--- packages/extended-text-entry/src/main.jsx
+++ packages/extended-text-entry/src/main.jsx
@@ -127,6 +127,7 @@ export class Main extends React.Component {
           highlightShape={true}
           toolbarOpts={toolbarOpts}
           spellCheck={spellCheckEnabled}
+          charactersLimit={50000}
           pluginProps={{
             math: {
               disabled: !mathInput,
