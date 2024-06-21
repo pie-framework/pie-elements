219a7365b61e6c4d9e76fbc06601e59ea65af115
Diana Gociu <diana.gociu@yahoo.com>
2024-06-21 19:40:56 +0300

    feat(math-templated): Added unit tests for player side PD-3807

---
+++ packages/math-templated/src/__tests__/main.test.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Main from '../Main'; // Adjust the import path as necessary
+import { withStyles } from '@material-ui/core/styles';
+import { Collapsible, Readable, hasText, PreviewPrompt } from '@pie-lib/pie-toolbox/render-ui';
+import MathQuill from '@pie-framework/mathquill';
+import { CorrectAnswerToggle } from '@pie-lib/pie-toolbox/correct-answer-toggle';
+
+// Utility function to simulate the toggle click and return the updated component
+const simulateToggleClick = (wrapper, toggleClassName) => {
+    const toggleContent = wrapper.find(toggleClassName).first();
+    toggleContent.simulate('click');
+    wrapper.update();
+    return wrapper.find(CorrectAnswerToggle).first();
+};
+
+const defaultModel = {
+    prompt: 'Solve the equation:',
+    teacherInstructions: '',
+    rationale: '',
+    responses: {
+        0: {
+            allowTrailingZeros: false,  
+            answer: "2",    
+            id: "1",
+            validation: "symbolic",
+            ignoreOrder: false,
+            alternates: {},
+        },
+        1: {
+            allowTrailingZeros: false,
+            answer: "5",
+            id: "2",
+            validation: "symbolic",
+            ignoreOrder: false,
+            alternates: {},
+        }
+        
+    },
+    env: {mode: 'evaluate', role: 'instructor'},
+    markup: '<p>If the unit price of a notebook in Store A is $1.50, what is the unit price of a binder? ${{0}} ${{1}}</p',
+    equationEditor: 'miscellaneous',
+    customKeys: [],
+    view: false,
+    feedback: {},
+    correctness: {
+        correct: false,
+        correctness: "incorrect",
+        score: 0
+    }
+};
+
+const defaultSession = {
+    answers: {
+        r0: {
+            value: "2"
+        },
+        r1: {
+            value: "3"
+        }
+    },
+    showCorrectness: true
+};
+
+const defaultProps = {
+    model: defaultModel,
+    session: defaultSession,
+    onSessionChange: jest.fn(),
+    classes: {},
+};
+
+describe('Main component', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(<Main {...defaultProps} />);
+    });
+    
+    it('Match Snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('displays the prompt', () => {
+        const prompt = 'Solve the equation:';
+        const wrapper = mount(<Main {...defaultProps} model={{ ...defaultModel, prompt }} />);
+        expect(wrapper.find(PreviewPrompt).text()).toContain(prompt);
+    });
+
+    it('shows on "evaluate" response areas with correct answers', () => {
+        const firstResponseArea = wrapper.find('Static').at(0);
+        const secondResponseArea = wrapper.find('Static').at(1);
+        expect(firstResponseArea.prop('latex')).toContain('[r0]{2}');
+        expect(secondResponseArea.prop('latex')).toContain('[r1]{3}');
+    });
+    
+    it('updates component when receiving new props', () => {
+        const newMarkup = 'Solve {{0}}';
+        wrapper.setProps({ model: { ...defaultModel, markup: newMarkup } });
+        expect(wrapper.text()).toContain('Solve');
+    });
+
+    it('updates session when change value in a response area', () => {
+        const wrapper = mount(<Main {...defaultProps} model={{ ...defaultModel, env: {mode: 'gather', role:'student' }}} />);
+        const firstResponseArea = wrapper.find('Static').at(0);
+        const onSubFieldChange = firstResponseArea.prop('onSubFieldChange');
+        onSubFieldChange('r1','5');
+
+        expect(defaultProps.onSessionChange).toHaveBeenCalledWith({
+            ...defaultProps.session,
+            answers: { r0: { value: '2' }, r1: { value: '5'} }
+        });
+    });
+
+    it('updates component when receiving new props', () => {
+        wrapper.setState({ showCorrect: true });
+        const toggle = wrapper.find(CorrectAnswerToggle);
+        const firstResponseArea = wrapper.find('Static').at(0);
+        const secondResponseArea = wrapper.find('Static').at(1);
+        expect(firstResponseArea.prop('latex')).toContain('[r0]{2}');
+        expect(secondResponseArea.prop('latex')).toContain('[r1]{3}');
+    });
+    
+    it('toggles CorrectAnswerToggle correctly', () => {
+        const updatedToggleComponent = simulateToggleClick(wrapper, '.CorrectAnswerToggle-content-26');
+        expect(updatedToggleComponent.props().toggled).toBe(true);
+    });
+
+    it('show orrect answers', () => {
+        const updatedToggleComponent = simulateToggleClick(wrapper, '.CorrectAnswerToggle-content-26');
+        const firstResponseArea = wrapper.find('Static').at(0);
+        const secondResponseArea = wrapper.find('Static').at(1);
+        expect(firstResponseArea.prop('latex')).toContain('2');
+        expect(secondResponseArea.prop('latex')).toContain('5');
+    });
+});
