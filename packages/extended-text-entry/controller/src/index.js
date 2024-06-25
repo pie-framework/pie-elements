ee44ea5053aeefeeb261183b5b56fe7b3b074f13
carlacostea <carlacostea@yahoo.com>
2024-06-25 11:53:29 +0200

    cleanup

--- packages/extended-text-entry/controller/src/index.js
+++ packages/extended-text-entry/controller/src/index.js
@@ -10,7 +10,6 @@ export async function createDefaultModel(model = {}) {
   return { ...defaults, ...model };
 }
 
-
 export const normalize = (question) => ({ ...defaults, ...question });
 
 export async function model(question, session, env) {
