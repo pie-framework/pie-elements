ead6fe3eda5d455d9e6d1ebda37ded18e54fa815
Andrei Miron <andrei.miron@mcro-e.com>
2024-12-19 15:52:42 +0200

    feat(multiple-choice): add min/max selection possibility PD-3843 PD-4518

--- packages/multiple-choice/src/multiple-choice.jsx
+++ packages/multiple-choice/src/multiple-choice.jsx
@@ -4,118 +4,132 @@ import { CorrectAnswerToggle } from '@pie-lib/pie-toolbox/correct-answer-toggle'
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import { color, Collapsible, PreviewPrompt } from '@pie-lib/pie-toolbox/render-ui';
+import Translator from '@pie-lib/pie-toolbox/translator';
 
 import StyledChoice from './choice';
 
 // MultipleChoice
 
+const { translator } = Translator;
+
 const styles = (theme) => ({
-    main: {
-        color: color.text(),
-        backgroundColor: color.background(),
-        '& *': {
-            '-webkit-font-smoothing': 'antialiased',
-        },
+  main: {
+    color: color.text(),
+    backgroundColor: color.background(),
+    '& *': {
+      '-webkit-font-smoothing': 'antialiased',
     },
-    partLabel: {
-        display: 'block',
-        fontSize: 'inherit',
-        margin: '0',
-        fontWeight: 'normal',
-        paddingBottom: theme.spacing.unit * 2,
-    },
-    teacherInstructions: {
-        marginBottom: theme.spacing.unit * 2,
-    },
-    horizontalLayout: {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-    },
-    gridLayout: {
-        display: 'grid',
-    },
-    fieldset: {
-        border: '0px',
-        padding: '0.01em 0 0 0',
-        margin: '0px',
-        minWidth: '0px',
-    },
-    srOnly: {
-        position: 'absolute',
-        left: '-10000px',
-        top: 'auto',
-        width: '1px',
-        height: '1px',
-        overflow: 'hidden',
-    },
+  },
+  partLabel: {
+    display: 'block',
+    fontSize: 'inherit',
+    margin: '0',
+    fontWeight: 'normal',
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  teacherInstructions: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  horizontalLayout: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  gridLayout: {
+    display: 'grid',
+  },
+  fieldset: {
+    border: '0px',
+    padding: '0.01em 0 0 0',
+    margin: '0px',
+    minWidth: '0px',
+  },
+  srOnly: {
+    position: 'absolute',
+    left: '-10000px',
+    top: 'auto',
+    width: '1px',
+    height: '1px',
+    overflow: 'hidden',
+  },
+  errorText: {
+    fontSize: theme.typography.fontSize - 2,
+    color: theme.palette.error.main,
+    paddingTop: theme.spacing.unit,
+  },
 });
 
 export class MultipleChoice extends React.Component {
-    static propTypes = {
-        className: PropTypes.string,
-        mode: PropTypes.oneOf(['gather', 'view', 'evaluate']),
-        choiceMode: PropTypes.oneOf(['radio', 'checkbox']),
-        keyMode: PropTypes.oneOf(['numbers', 'letters', 'none']),
-        choices: PropTypes.array,
-        partLabel: PropTypes.string,
-        prompt: PropTypes.string,
-        teacherInstructions: PropTypes.string,
-        session: PropTypes.object,
-        disabled: PropTypes.bool,
-        onChoiceChanged: PropTypes.func,
-        responseCorrect: PropTypes.bool,
-        classes: PropTypes.object.isRequired,
-        correctResponse: PropTypes.array,
-        choicesLayout: PropTypes.oneOf(['vertical', 'grid', 'horizontal']),
-        gridColumns: PropTypes.string,
-        alwaysShowCorrect: PropTypes.bool,
-        animationsDisabled: PropTypes.bool,
-        language: PropTypes.string,
-        onShowCorrectToggle: PropTypes.func,
-        isSelectionButtonBelow: PropTypes.bool,
+  static propTypes = {
+    className: PropTypes.string,
+    mode: PropTypes.oneOf(['gather', 'view', 'evaluate']),
+    choiceMode: PropTypes.oneOf(['radio', 'checkbox']),
+    keyMode: PropTypes.oneOf(['numbers', 'letters', 'none']),
+    choices: PropTypes.array,
+    partLabel: PropTypes.string,
+    prompt: PropTypes.string,
+    teacherInstructions: PropTypes.string,
+    session: PropTypes.object,
+    disabled: PropTypes.bool,
+    onChoiceChanged: PropTypes.func,
+    responseCorrect: PropTypes.bool,
+    classes: PropTypes.object.isRequired,
+    correctResponse: PropTypes.array,
+    choicesLayout: PropTypes.oneOf(['vertical', 'grid', 'horizontal']),
+    gridColumns: PropTypes.string,
+    alwaysShowCorrect: PropTypes.bool,
+    animationsDisabled: PropTypes.bool,
+    language: PropTypes.string,
+    onShowCorrectToggle: PropTypes.func,
+    isSelectionButtonBelow: PropTypes.bool,
+    minSelections: PropTypes.number,
+    maxSelections: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedValue: null,
+      selectedValues: [],
+      showCorrect: this.props.alwaysShowCorrect || false,
     };
 
-    constructor(props) {
-        super(props);
+    this.onToggle = this.onToggle.bind(this);
+  }
 
-        this.state = {
-            selectedValue: null,
-            selectedValues: [],
-            showCorrect: this.props.alwaysShowCorrect || false,
-        };
+  isSelected(value) {
+    const sessionValue = this.props.session && this.props.session.value;
 
-        this.onToggle = this.onToggle.bind(this);
+    return sessionValue && sessionValue.indexOf && sessionValue.indexOf(value) >= 0;
+  }
+
+  // handleChange was added for accessibility. Please see comments and videos from PD-2441.
+  // TODO: Should only be removed if a better solution is found.
+  handleChange = (event) => {
+    const { value, checked } = event.target;
+    const { choiceMode, maxSelections, onChoiceChanged } = this.props;
+
+    if (choiceMode === 'radio') {
+      this.setState({ selectedValue: value });
+    } else {
+      const { selectedValues } = this.state;
+
+      if (checked && selectedValues.length >= maxSelections) {
+        return;
+      }
+
+      this.setState((prevState) => {
+        const selectedValues = checked
+          ? [...prevState.selectedValues, value]
+          : prevState.selectedValues.filter((currentValue) => currentValue !== value);
+
+        return { selectedValues };
+      });
     }
 
-    isSelected(value) {
-        const sessionValue = this.props.session && this.props.session.value;
-
-        return sessionValue && sessionValue.indexOf && sessionValue.indexOf(value) >= 0;
-    }
-
-    // handleChange and handleChangeCheckboxes functions were added for accessibility. Please see comments and videos from PD-2441. They should only be removed if a better solution is found.
-    handleChange = (event) => {
-        const target = event.target;
-
-        this.setState({selectedValue: target.value});
-    };
-
-    handleChangeCheckboxes = (event) => {
-        const {value, checked} = event.target;
-
-        this.setState((prevState) => {
-            let selectedValues = [...prevState.selectedValues];
-
-            if (checked) {
-                selectedValues.push(value);
-            } else {
-                selectedValues = selectedValues.filter((currentValue) => currentValue !== value);
-            }
-
-            return {selectedValues};
-        });
-    };
+    onChoiceChanged({ value, selected: checked });
+  };
 
   onToggle = () => {
     if (this.props.mode === 'evaluate') {
@@ -145,181 +159,200 @@ export class MultipleChoice extends React.Component {
     }
   }
 
-    indexToSymbol(index) {
-        if (this.props.keyMode === 'numbers') {
-            return `${index + 1}`;
-        }
-
-        if (this.props.keyMode === 'letters') {
-            return String.fromCharCode(97 + index).toUpperCase();
-        }
-
-        return '';
+  indexToSymbol(index) {
+    if (this.props.keyMode === 'numbers') {
+      return `${index + 1}`;
     }
 
-    getCorrectness = (choice = {}) => {
-        const isCorrect = choice.correct;
-        const isChecked = this.isSelected(choice.value);
-
-        if (this.state.showCorrect) {
-            return isCorrect ? 'correct' : undefined;
-        }
-
-        if (isCorrect) {
-            if (isChecked) {
-                // A correct answer is selected: marked with a green checkmark
-                return 'correct';
-            } else {
-                // A correct answer is NOT selected: marked with an orange X
-                return 'incorrect';
-            }
-        } else {
-            if (isChecked) {
-                // An incorrect answer is selected: marked with an orange X
-                return 'incorrect';
-            } else {
-                // An incorrect answer is NOT selected: not marked
-                return undefined;
-            }
-        }
-    };
-
-    getChecked(choice) {
-        if (this.state.showCorrect) {
-            return choice.correct || false;
-        }
-
-        if (this.isSelected(choice.value)) {
-            return true;
-        }
-
-        return this.props.choiceMode === 'radio'
-            ? this.state.selectedValue === choice.value
-            : this.state.selectedValues.includes(choice.value);
+    if (this.props.keyMode === 'letters') {
+      return String.fromCharCode(97 + index).toUpperCase();
     }
 
-    // renderHeading function was added for accessibility.
-    renderHeading() {
-        const {mode, choiceMode, classes} = this.props;
+    return '';
+  }
 
-        if (mode !== 'gather') {
-            return null;
-        }
+  getCorrectness = (choice = {}) => {
+    const isCorrect = choice.correct;
+    const isChecked = this.isSelected(choice.value);
 
-        return choiceMode === 'radio' ? (
-            <h2 className={classes.srOnly}>Multiple Choice Question</h2>
-        ) : (
-            <h2 className={classes.srOnly}>Multiple Select Question</h2>
-        );
+    if (this.state.showCorrect) {
+      return isCorrect ? 'correct' : undefined;
     }
 
-    render() {
-        const {
-            mode,
-            disabled,
-            className,
-            choices = [],
-            choiceMode,
-            gridColumns,
-            partLabel,
-            prompt,
-            onChoiceChanged,
-            responseCorrect,
-            teacherInstructions,
-            classes,
-            alwaysShowCorrect,
-            animationsDisabled,
-            language,
-            isSelectionButtonBelow
-        } = this.props;
-        const {showCorrect} = this.state;
-        const isEvaluateMode = mode === 'evaluate';
-        const showCorrectAnswerToggle = isEvaluateMode && !responseCorrect;
-        const columnsStyle = gridColumns > 1 ? {gridTemplateColumns: `repeat(${gridColumns}, 1fr)`} : undefined;
+    if (isCorrect) {
+      if (isChecked) {
+        // A correct answer is selected: marked with a green checkmark
+        return 'correct';
+      } else {
+        // A correct answer is NOT selected: marked with an orange X
+        return 'incorrect';
+      }
+    } else {
+      if (isChecked) {
+        // An incorrect answer is selected: marked with an orange X
+        return 'incorrect';
+      } else {
+        // An incorrect answer is NOT selected: not marked
+        return undefined;
+      }
+    }
+  };
 
-        const teacherInstructionsDiv = (
-            <PreviewPrompt
-                tagName="div"
-                className="prompt"
-                defaultClassName="teacher-instructions"
-                prompt={teacherInstructions}
+  getChecked(choice) {
+    if (this.state.showCorrect) {
+      return choice.correct || false;
+    }
+
+    if (this.isSelected(choice.value)) {
+      return true;
+    }
+
+    return this.props.choiceMode === 'radio'
+      ? this.state.selectedValue === choice.value
+      : this.state.selectedValues.includes(choice.value);
+  }
+
+  // renderHeading function was added for accessibility.
+  renderHeading() {
+    const { mode, choiceMode, classes } = this.props;
+
+    if (mode !== 'gather') {
+      return null;
+    }
+
+    return choiceMode === 'radio' ? (
+      <h2 className={classes.srOnly}>Multiple Choice Question</h2>
+    ) : (
+      <h2 className={classes.srOnly}>Multiple Select Question</h2>
+    );
+  }
+
+  render() {
+    const {
+      mode,
+      disabled,
+      className,
+      choices = [],
+      choiceMode,
+      gridColumns,
+      partLabel,
+      prompt,
+      responseCorrect,
+      teacherInstructions,
+      classes,
+      alwaysShowCorrect,
+      animationsDisabled,
+      language,
+      isSelectionButtonBelow,
+      minSelections,
+      maxSelections,
+      session,
+    } = this.props;
+    const { showCorrect } = this.state;
+    const isEvaluateMode = mode === 'evaluate';
+    const showCorrectAnswerToggle = isEvaluateMode && !responseCorrect;
+    const columnsStyle = gridColumns > 1 ? { gridTemplateColumns: `repeat(${gridColumns}, 1fr)` } : undefined;
+    const selections = (session.value && session.value.length) || 0;
+
+    const teacherInstructionsDiv = (
+      <PreviewPrompt
+        tagName="div"
+        className="prompt"
+        defaultClassName="teacher-instructions"
+        prompt={teacherInstructions}
+      />
+    );
+
+    return (
+      <div className={classNames(classes.main, className, 'multiple-choice')}>
+        {partLabel && <h3 className={classes.partLabel}>{partLabel}</h3>}
+
+        {this.renderHeading()}
+
+        {teacherInstructions && (
+          <div className={classes.teacherInstructions}>
+            {!animationsDisabled ? (
+              <Collapsible
+                labels={{
+                  hidden: 'Show Teacher Instructions',
+                  visible: 'Hide Teacher Instructions',
+                }}
+              >
+                {teacherInstructionsDiv}
+              </Collapsible>
+            ) : (
+              teacherInstructionsDiv
+            )}
+          </div>
+        )}
+
+        <fieldset className={classes.fieldset}>
+          <PreviewPrompt className="prompt" defaultClassName="prompt" prompt={prompt} tagName={'legend'} />
+
+          {!alwaysShowCorrect && (
+            <CorrectAnswerToggle
+              show={showCorrectAnswerToggle}
+              toggled={showCorrect}
+              onToggle={this.onToggle.bind(this)}
+              language={language}
             />
-        );
+          )}
 
-        return (
-            <div className={classNames(classes.main, className, 'multiple-choice')}>
-                {partLabel && <h3 className={classes.partLabel}>{partLabel}</h3>}
+          <div
+            className={classNames({
+              [classes.gridLayout]: this.props.choicesLayout === 'grid',
+              [classes.horizontalLayout]: this.props.choicesLayout === 'horizontal',
+            })}
+            style={columnsStyle}
+          >
+            {choices.map((choice, index) => (
+              <StyledChoice
+                choicesLayout={this.props.choicesLayout}
+                gridColumns={gridColumns}
+                key={`choice-${index}`}
+                choice={choice}
+                index={index}
+                choicesLength={choices.length}
+                showCorrect={showCorrect}
+                isEvaluateMode={isEvaluateMode}
+                choiceMode={choiceMode}
+                disabled={disabled}
+                onChoiceChanged={this.handleChange}
+                hideTick={choice.hideTick}
+                checked={this.getChecked(choice)}
+                correctness={isEvaluateMode ? this.getCorrectness(choice) : undefined}
+                displayKey={this.indexToSymbol(index)}
+                isSelectionButtonBelow={isSelectionButtonBelow}
+              />
+            ))}
+          </div>
+        </fieldset>
 
-                {this.renderHeading()}
-
-                {teacherInstructions && (
-                    <div className={classes.teacherInstructions}>
-                        {!animationsDisabled ? (
-                            <Collapsible
-                                labels={{
-                                    hidden: 'Show Teacher Instructions',
-                                    visible: 'Hide Teacher Instructions',
-                                }}
-                            >
-                                {teacherInstructionsDiv}
-                            </Collapsible>
-                        ) : (
-                            teacherInstructionsDiv
-                        )}
-                    </div>
-                )}
-
-                <fieldset className={classes.fieldset}>
-                    <PreviewPrompt className="prompt" defaultClassName="prompt" prompt={prompt} tagName={'legend'}/>
-
-                    {!alwaysShowCorrect && (
-                        <CorrectAnswerToggle
-                            show={showCorrectAnswerToggle}
-                            toggled={showCorrect}
-                            onToggle={this.onToggle.bind(this)}
-                            language={language}
-                        />
-                    )}
-
-                    <div
-                        className={classNames({
-                            [classes.gridLayout]: this.props.choicesLayout === 'grid',
-                            [classes.horizontalLayout]: this.props.choicesLayout === 'horizontal',
-                        })}
-                        style={columnsStyle}
-                    >
-                        {choices.map((choice, index) => (
-                            <StyledChoice
-                                choicesLayout={this.props.choicesLayout}
-                                gridColumns={gridColumns}
-                                key={`choice-${index}`}
-                                choice={choice}
-                                index={index}
-                                choicesLength={choices.length}
-                                showCorrect={showCorrect}
-                                isEvaluateMode={isEvaluateMode}
-                                choiceMode={choiceMode}
-                                disabled={disabled}
-                                updateSession={onChoiceChanged}
-                                onChoiceChanged={this.props.choiceMode === 'radio' ? this.handleChange : this.handleChangeCheckboxes}
-                                hideTick={choice.hideTick}
-                                checked={this.getChecked(choice)}
-                                correctness={isEvaluateMode ? this.getCorrectness(choice) : undefined}
-                                displayKey={this.indexToSymbol(index)}
-                                isSelectionButtonBelow={isSelectionButtonBelow}
-                            />
-                        ))}
-                    </div>
-                </fieldset>
-            </div>
-        );
-    }
+        {choiceMode === 'checkbox' && selections < minSelections && (
+          <div className={classes.errorText}>
+            {translator.t(`translation:multipleChoice:minSelections_${minSelections === 1 ? 'one' : 'other'}`, {
+              lng: language,
+              minSelections,
+            })}
+          </div>
+        )}
+        {choiceMode === 'checkbox' && selections >= maxSelections && (
+          <div className={classes.errorText}>
+            {translator.t(`translation:multipleChoice:maxSelections_${maxSelections === 1 ? 'one' : 'other'}`, {
+              lng: language,
+              maxSelections,
+            })}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 MultipleChoice.defaultProps = {
-    session: {
-        value: [],
-    },
+  session: {
+    value: [],
+  },
 };
 
 export default withStyles(styles)(MultipleChoice);
