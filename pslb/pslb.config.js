56312ea13151cc7e5d026a112fb7d86d320e4799
carlacostea <carlacostea@yahoo.com>
2025-04-10 15:55:39 +0300

    fix: update pie-lib/pie-toolbox PD-4765, PD-4947

--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -46,7 +46,7 @@ module.exports = {
   libs: {
     repository: 'pie-framework/pie-elements',
     packages: [
-      { name: '@pie-lib/pie-toolbox-module', version: '5.13.0' }
+      { name: '@pie-lib/pie-toolbox-module', version: '5.14.0' }
     ],
   },
 };
