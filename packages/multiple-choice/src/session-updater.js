b74e9acb3e7b700f8c3768b79a7a54e9752b26d9
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2025-06-17 14:30:09 +0300

    refactor(multiple-choice): replace lodash methods with native array methods for session value updates PD-5063

--- packages/multiple-choice/src/session-updater.js
+++ packages/multiple-choice/src/session-updater.js
@@ -1,14 +1,10 @@
-import concat from 'lodash/concat';
-import uniq from 'lodash/uniq';
-import without from 'lodash/without';
-
 export function updateSessionValue(session, choiceMode, data) {
   session.value = session.value || [];
   if (choiceMode === 'checkbox') {
     if (data.selected) {
-      session.value = uniq(concat(session.value, [data.value]));
+      session.value = Array.from(new Set([...session.value, data.value]));
     } else {
-      session.value = without(session.value, data.value);
+      session.value = session.value.filter(v => v !== data.value);
     }
   }
 
