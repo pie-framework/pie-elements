ab529fe21aff6c27e23b1db977c958de85b46eea
carlacostea <carlacostea@yahoo.com>
2025-06-25 17:06:17 +0300

    fix: use demo model with math content for accessibility testing

--- packages/multiple-choice/docs/demo/generate.js
+++ packages/multiple-choice/docs/demo/generate.js
@@ -1,61 +1,37 @@
 exports.model = (id, element) => ({
   id,
   element,
-  choiceMode: 'checkbox',
-  choicePrefix: 'numbers',
+  keyMode: 'none',
+  lockChoiceOrder: true,
+  choiceMode: 'radio',
   choices: [
     {
-      correct: true,
-      value: 'sweden',
-      label: 'Sweden',
-      feedback: {
-        type: 'none',
-        value: '',
-      },
+      correct: false,
+      label: '<math><mi>a</mi><mo>=</mo><mroot><mn>4</mn><mn>3</mn></mroot></math>',
+      value: '1',
+      rationale:
+        'Student(s) may have correctly found the cube root of 64, but they may have put it under a cube root sign.',
     },
     {
-      value: 'iceland',
-      label: 'Iceland',
-      feedback: {
-        type: 'none',
-        value: '',
-      },
-      rationale: 'Rationale for Iceland',
-    },
-    {
-      value: 'norway',
-      label: 'Norway',
-      feedback: {
-        type: 'none',
-        value: '',
-      },
-      rationale: 'Rationale for Norway',
+      correct: false,
+      label: '<math><mi>a</mi><mo>=</mo><msqrt><mn>8</mn></msqrt></math>',
+      value: '2',
+      rationale:
+        'Student(s) may have mistaken the exponent of 3 for an exponent of 2 and may have correctly found the square root of 64 and put it under a square root sign.',
     },
     {
       correct: true,
-      value: 'finland',
-      label: 'Finland',
-      feedback: {
-        type: 'none',
-        value: '',
-      },
-      rationale: 'Rationale for Finland',
+      label: '<math><mi>a</mi><mo>=</mo><mroot><mn>64</mn><mn>3</mn></mroot></math>',
+      value: '3',
+      rationale: 'Correct answer',
+    },
+    {
+      correct: false,
+      label: '<math><mi>a</mi><mo>=</mo><msqrt><mn>64</mn></msqrt></math>',
+      value: '4',
+      rationale: 'Student(s) may have mistaken the exponent of 3 for an exponent of 2.',
     },
   ],
-  extraCSSRules: {
-    names: ['red', 'blue'],
-    rules: `
-      .red {
-        color: red !important;
-      }
-
-      .blue {
-        color: blue !important;
-      }
-    `,
-  },
-  prompt: '',
-  promptEnabled: true,
-  toolbarEditorPosition: 'bottom',
-  rubricEnabled: false,
+  prompt:
+    'Consider the equation <math><msup><mi>a</mi><mn>3</mn></msup><mo>=</mo><mn>64</mn></math>.<br>\n<br>\nPart A:<br>\n<br>\nWhat is the solution to the equation?',
 });
