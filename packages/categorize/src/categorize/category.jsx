59fd09dcb01a8c6013fdba9cd82c80b03b44e3d9
arimieandreea <arimie_andreea@yahoo.com>
2026-01-29 16:49:20 +0200

    fix(categorize): applies styled to placeholder using extrastyles PD-5494

--- packages/categorize/src/categorize/category.jsx
+++ packages/categorize/src/categorize/category.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
-import { color } from '@pie-lib/render-ui';
 
 import Choice from './choice';
 import PlaceHolder from './droppable-placeholder';
@@ -37,7 +36,7 @@ export class Category extends React.Component {
 
     return (
       <StyledDiv className={className} id={id}>
-        <StyledPlaceHolder
+        <PlaceHolder
           id={id}
           onDropChoice={onDropChoice}
           disabled={disabled}
@@ -54,7 +53,7 @@ export class Category extends React.Component {
               {...c}
             />
           ))}
-        </StyledPlaceHolder>
+        </PlaceHolder>
       </StyledDiv>
     );
   }
@@ -64,26 +63,6 @@ const StyledDiv = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   flex: 2,
-}));
-
-const StyledPlaceHolder = styled(PlaceHolder, {
-  shouldForwardProp: (prop) => prop !== 'correct',
-})(({ theme, correct }) => ({
-  padding: theme.spacing(0.5),
-  borderRadius: theme.spacing(0.5),
-  gridColumnGap: 0,
-  gridRowGap: 0,
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  alignItems: 'center',
-  alignContent: 'flex-start',
-  ...(correct === false && {
-    border: `solid 2px ${color.incorrect()}`,
-  }),
-  ...(correct === true && {
-    border: `solid 2px ${color.correct()}`,
-  }),
 }));
 
 export default Category;
