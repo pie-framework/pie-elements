bb1f3eb4e3b5b1cb15024d459ffd6e94bf419712
carlacostea <carlacostea@yahoo.com>
2026-02-06 13:57:39 +0200

    feat: bump pslb shared modules

--- pslb/pslb.config.js
+++ pslb/pslb.config.js
@@ -46,12 +46,12 @@ module.exports = {
   libs: {
     repository: 'pie-framework/pie-elements',
     packages: [
-      { name: '@pie-lib/drag-module', version: '^3.0.0' },
-      { name: '@pie-lib/math-rendering-module', version: '^4.0.0' },
-      { name: '@pie-lib/math-edit-module', version: '^3.0.0' },
-      { name: '@pie-lib/shared-module', version: '^3.0.0' },
-      { name: '@pie-lib/editable-html-module', version: '^6.0.0' },
-      { name: '@pie-lib/config-module', version: '^2.23.3' },
+      { name: '@pie-lib/drag-module', version: '^3.0.5' },
+      { name: '@pie-lib/math-rendering-module', version: '^4.0.5' },
+      { name: '@pie-lib/math-edit-module', version: '^3.0.5' },
+      { name: '@pie-lib/shared-module', version: '^3.0.5' },
+      { name: '@pie-lib/editable-html-module', version: '^6.0.5' },
+      { name: '@pie-lib/config-module', version: '^3.0.0' },
     ],
   },
 };
