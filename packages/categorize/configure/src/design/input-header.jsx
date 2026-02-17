39b7a9b7aed3abcb4c6023829f1b2448631ed23b
carlacostea <carlacostea@yahoo.com>
2026-02-17 10:20:56 +0200

    fix(categorize): resolve crash when adding new category and implement auto-focus PD-5588

--- packages/categorize/configure/src/design/input-header.jsx
+++ packages/categorize/configure/src/design/input-header.jsx
@@ -46,13 +46,6 @@ export class InputHeader extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    const { focusedEl, index } = this.props;
-    if (focusedEl && index && focusedEl === index) {
-      this.inputRef.focus('end', null, true);
-    }
-  }
-
   render() {
     const {
       onChange,
@@ -68,16 +61,19 @@ export class InputHeader extends React.Component {
       maxImageHeight,
       uploadSoundSupport,
       mathMlOptions = {},
+      focusedEl,
+      index,
     } = this.props;
 
     const { headers, baseInputConfiguration } = configuration;
+    const shouldAutoFocus = focusedEl !== null && focusedEl !== undefined && focusedEl === index;
 
     return (
       <InputHeaderContainer>
         <StyledEditableHtml
           imageSupport={imageSupport}
           disabled={disabled}
-          ref={(ref) => (this.inputRef = ref)}
+          autoFocus={shouldAutoFocus}
           autoWidthToolbar
           label={'label'}
           markup={label}
