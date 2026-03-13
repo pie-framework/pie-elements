8a4ac213977a3ddda2a73abb9586fb765eaa8c50
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2026-03-13 17:39:32 +0200

    fix(passage): use basic plugin filtering and add math rendering observer PD-5789

--- packages/passage/configure/src/passage.jsx
+++ packages/passage/configure/src/passage.jsx
@@ -84,6 +84,13 @@ export class PassageComponent extends React.Component {
 
     const getPluginProps = (customConfiguration) => ({ ...baseInputConfiguration, ...customConfiguration });
 
+    const basicPlugins = ALL_PLUGINS.filter(plugin => ![
+      'math',
+      'table',
+      'bulleted-list',
+      'numbered-list',
+    ].includes(plugin));
+
     return (
       <React.Fragment>
         {teacherInstructionsEnabled && (
@@ -107,7 +114,7 @@ export class PassageComponent extends React.Component {
         {titleEnabled && (
           <StyledInputContainer label={title.label}>
             <EditableHtml
-              activePlugins={ALL_PLUGINS}
+              activePlugins={basicPlugins}
               markup={passages[passageIndex].title || ''}
               onChange={(value) => this.handleChange('title', value)}
               nonEmpty={false}
@@ -123,7 +130,7 @@ export class PassageComponent extends React.Component {
         {subtitleEnabled && (
           <StyledInputContainer label={subtitle.label}>
             <EditableHtml
-              activePlugins={ALL_PLUGINS}
+              activePlugins={basicPlugins}
               markup={passages[passageIndex].subtitle || ''}
               onChange={(value) => this.handleChange('subtitle', value)}
               nonEmpty={false}
@@ -139,7 +146,7 @@ export class PassageComponent extends React.Component {
         {authorEnabled && (
           <StyledInputContainer label={author.label}>
             <EditableHtml
-              activePlugins={ALL_PLUGINS}
+              activePlugins={basicPlugins}
               markup={passages[passageIndex].author || ''}
               onChange={(value) => this.handleChange('author', value)}
               nonEmpty={false}
