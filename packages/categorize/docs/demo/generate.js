d08c28fdc1aead7a4ed58a79309177467f1cacd0
arimieandreea <arimie_andreea@yahoo.com>
2026-03-24 10:13:36 +0200

    fix(categorize): set back old model from docs PD-5514

--- packages/categorize/docs/demo/generate.js
+++ packages/categorize/docs/demo/generate.js
@@ -279,90 +279,79 @@
 exports.model = (id, element) => ({
   id,
   element,
-  promptEnabled: true,
-  choices: [
+  'teacherInstructions': '',
+  'promptEnabled': true,
+  'prompt': '<p><audio controls="" controlslist="nodownload" id="2ad0337f-904c-4cd6-b6d3-399c08db8642.mp3" xmlns="http://www.w3.org/1999/html"><source src="https://assets.pie-api.com/assets/c9946808-ab26-45d9-bd98-f430c97f09fc/audio/mpeg/6ab9652e-7277-4a12-8f7a-a2bfb76f127c" type="audio/mpeg">Your browser does not support the audio element.</audio></p><p>Move the equations to tell whether they are true or false.</p>',
+  'categories': [
     {
-      id: '0',
-      content:
-        '<div><span data-latex="" data-raw="420\\text{ cm}=4.2\\text{ meters}">420\\text{ cm}=4.2\\text{ meters}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 1,
+      'id': '0',
+      'label': '<img height="150" src="https://assets.pie-api.com/assets/c9946808-ab26-45d9-bd98-f430c97f09fc/image/svg xml/ab759120-d9b0-4f6f-b755-f4f83f6bc0f7" width="265">'
     },
     {
-      id: '1',
-      content:
-        '<div><span data-latex="" data-raw="3.4\\text{ kg}=340\\text{ g}">3.4\\text{ kg}=340\\text{ g}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 1,
-    },
-    {
-      id: '2',
-      content:
-        '<div><span data-latex="" data-raw="1\\text{,}800\\text{ mL}=180\\text{ L}">1\\text{,}800\\text{ mL}=180\\text{ L}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 0,
-    },
-    {
-      id: '3',
-      content:
-        '<div><span data-latex="" data-raw="3.5\\text{ meters}=350\\text{ cm}">3.5\\text{ meters}=350\\text{ cm}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 1,
-    },
-    {
-      id: '4',
-      content:
-        '<div><span data-latex="" data-raw="4\\text{,}800\\text{ g}=0.48\\text{ kg}">4\\text{,}800\\text{ g}=0.48\\text{ kg}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 1,
-    },
-    {
-      id: '5',
-      content:
-        '<div><span data-latex="" data-raw="250\\text{ mL}=0.25\\text{ L}">250\\text{ mL}=0.25\\text{ L}</span></div>',
-      categoryCount: 0,
-      correctResponseCount: 0,
-    },
+      'id': '1',
+      'label': '<img height="150" src="https://assets.pie-api.com/assets/c9946808-ab26-45d9-bd98-f430c97f09fc/image/svg xml/9f2a6102-7acb-43cb-8651-d75d4aece3ae" width="265">'
+    }
   ],
-  categories: [
+  'categoriesPerRow': 2,
+  'allowMaxChoicesPerCategory': false,
+  'maxChoicesPerCategory': 0,
+  'rowLabels': [],
+  'choices': [
     {
-      id: '0',
-      label: '<div>Equivalent</div>',
+      'id': '0',
+      'content': '\\(6+3=9-2\\)',
+      'categoryCount': 1,
+      'correctResponseCount': 1
     },
     {
-      id: '1',
-      label: '<div><b>NOT </b>equivalent</div>',
+      'id': '1',
+      'content': '\\(10-4=9-5\\)',
+      'categoryCount': 1,
+      'correctResponseCount': 1
     },
+    {
+      'id': '2',
+      'content': '\\(17-9=9+17\\)',
+      'categoryCount': 1,
+      'correctResponseCount': 1
+    },
+    {
+      'id': '3',
+      'content': '\\(11+9=10+10\\)',
+      'categoryCount': 1,
+      'correctResponseCount': 1
+    },
+    {
+      'id': '4',
+      'content': '\\(14-4=5+5\\)',
+      'categoryCount': 1,
+      'correctResponseCount': 1
+    }
   ],
-  rowLabels: ['<div></div>'],
-  correctResponse: [
+  'choicesPosition': 'above',
+  'choicesLabel': '',
+  'lockChoiceOrder': true,
+  'allowMultiplePlacementsEnabled': 'No',
+  'correctResponse': [
     {
-      category: '0',
-      choices: ['0', '3'],
+      'category': '0',
+      'choices': [
+        '3',
+        '4'
+      ]
     },
     {
-      category: '1',
-      choices: ['4', '1'],
-    },
+      'category': '1',
+      'choices': [
+        '0',
+        '1',
+        '2'
+      ]
+    }
   ],
-  allowAlternateEnabled: true,
-  allowMaxChoicesPerCategory: false,
-  allowMultiplePlacementsEnabled: 'Yes',
-  alternates: [],
-  categoriesPerRow: 2,
-  choicesLabel: '',
-  choicesPosition: 'below',
-  feedbackEnabled: false,
-  lockChoiceOrder: true,
-  maxAnswerChoices: 6,
-  maxChoicesPerCategory: 0,
-  partialScoring: true,
-  rationaleEnabled: true,
-  studentInstructionsEnabled: true,
-  teacherInstructionsEnabled: true,
-  toolbarEditorPosition: 'bottom',
-  minRowHeight: '80px',
-  teacherInstructions: '<div></div>',
-  prompt: '<div></div>',
-  rationale: '<div></div>',
+  'allowAlternateEnabled': false,
+  'alternates': [],
+  'partialScoring': true,
+  'minRowHeight': '175px',
+  'fontSizeFactor': 1.3
 });
