b7dfd29e6d5f1981799f8cef96f6f73ae829951c
arimieandreea <arimie_andreea@yahoo.com>
2026-08-04 11:22:36 +0300

    fix(multiple-choice, number-line): add render math in configure PIE-763

--- packages/number-line/configure/src/index.js
+++ packages/number-line/configure/src/index.js
@@ -1,6 +1,7 @@
 import Main from './main';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { renderMath } from '@pie-lib/math-rendering';
 import {
   ModelUpdatedEvent,
   InsertSoundEvent,
@@ -179,6 +180,7 @@ export default class NumberLine extends HTMLElement {
       this._root = createRoot(this);
     }
     this._root.render(element);
+    setTimeout(() => renderMath(this), 0);
   }
 
   connectedCallback() {
